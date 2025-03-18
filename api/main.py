e1971982dedf4fcae259dc670b336e974b229d49
Angelo Viloria <lpviloria@up.edu.ph>
2025-03-18 21:15:35 +0800

    feat: edited main.py to test new features

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from routers import userlogs_routes, reports_routes
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+app.include_router(userlogs_routes.router)
+app.include_router(reports_routes.router)
