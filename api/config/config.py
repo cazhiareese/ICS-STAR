794420a6ec7ae7f63dd071694142be0543b17d28
pvgonzales <pvgonzales@up.edu.ph>
2025-03-19 01:37:20 +0800

    chore: setup and added database configuration

--- api/config/config.py
+++ api/config/config.py
@@ -1,3 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://{}")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+SECRET_KEY = "c85f7f6d5009815e7b69bf6eabc655fb1a2533d1f808fe1e950961f2b5add6da"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
