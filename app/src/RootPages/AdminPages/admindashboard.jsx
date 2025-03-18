9c901901468f1314a5a7ba5eabb3716d38309e58
ReddDL <redeleon.dev@gmail.com>
2025-03-19 00:58:25 +0800

    feat: created the structure of the admin dashboard

--- app/src/RootPages/AdminPages/admindashboard.jsx
+++ app/src/RootPages/AdminPages/admindashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Home, FileText, Calendar, Newspaper, BriefcaseBusiness, Heart, LogOut } from "lucide-react";
+import { Home, FileText, Calendar, Newspaper, BriefcaseBusiness, Heart, LogOut, CircleUserRound, FileUser, ShieldUser, TriangleAlert, HandCoins, MoveRight, Users, Wallet } from "lucide-react";
 import SidebarItem from "../../components/AdminComponents/sidebaritem";
 
 function AdminLanding() {
@@ -16,12 +16,20 @@ function AdminLanding() {
     { id: "donations", title: "Donations", icon: <Heart size={24} /> },    
   ];
 
-  const dashboardCard = "bg-white drop-shadow-sm rounded-2xl";
+  const response = {
+    pendingVerifications: 28,
+    reportedPostings: 5, 
+    reportedUsers: 12, 
+    fundingRequests: 10,
+    registeredAlumni: 542,
+  }
+
+  const dashboardCard = "bg-white drop-shadow-sm rounded-2xl p-4";
 
   return <>
-  <div className="flex h-screen w-screen">
+  <div className="flex flex-row">
     {/* Sidebar */}
-    <div className="bg-white h-screen flex flex-col gap-3 px-4 pt-20 min-w-56">
+    <div className="bg-white h-screen flex flex-col gap-3 px-4 pt-20 w-2/12">
       {/* Sidebar items */}
       {sidebarItems.map((item) => (
           <SidebarItem
@@ -38,30 +46,148 @@ function AdminLanding() {
         <p className="text-lg font-satoshi-medium">Log out</p>
       </div>
     </div>
-    {/* Dashboard */}
-    <div>
-      <div className="bg-[#F3F1F4] h-screen w-full grid grid-cols-4 grid-rows-[0.5fr_1fr_1fr_1fr_1fr_1fr_1fr] gap-4 p-4">
+
+    {/* Dashboard area */}
+    <div className="bg-[#F3F1F4] flex-1 overflow-auto">
+      <div className=" p-4 flex flex-col max-w-7xl mx-auto">
+        <div className="flex flex-row mb-3 items-center">
+          <CircleUserRound size={32}/>
+          <p className="ml-2 text-3xl">
+            <span className="font-satoshi-light">Welcome back </span>
+            <span className="font-satoshi-bold">Admin,</span>
+          </p>
+        </div>
+        {/* Grid */}
+        <div className="flex-1 grid grid-cols-4 grid-rows-[0.5fr_1fr_1fr_1.7fr_0.2fr_2fr] gap-4">
         {/* Dashboard Banner */}
-        <div className="col-span-4 row-start-1 rounded-2xl bg-cover bg-center bg-no-repeat bg-[url('/assets/DashboardBanner.svg')] flex items-center justify-between"> 
-          <p className="text-white font-satoshi-bold text-2xl ml-5"> Dashboard </p>
-          <p className="text-white font-satoshi-light mr-52"> Bridging Alumni across the Cosmos </p>
-        </div> 
+          <div className="col-span-4 row-start-1 rounded-2xl bg-cover bg-center bg-no-repeat bg-[url('/assets/DashboardBanner.svg')] flex items-center justify-between"> 
+            <p className="text-white font-satoshi-bold text-2xl ml-5"> Dashboard </p>
+            <p className="text-white font-satoshi-light mr-52"> Bridging Alumni across the Cosmos </p>
+          </div> 
         {/* Pending Verifications */}
-        <div className={`${dashboardCard} col-start-1 row-start-2`}> Pending Verifications </div> 
+          <div className={`${dashboardCard} col-start-1 row-start-2 flex flex-row justify-between`}> 
+            <div className="flex flex-col">
+              <p className="font-satoshi-light text-sm"> Pending Verifications </p>
+              <p className="text-5xl"> {response.pendingVerifications} </p>
+            </div>
+            <div className="">
+              <div className="w-12 h-12 rounded-full bg-black flex items-center justify-center">
+                <FileUser className="w-6 h-6 text-white" />
+              </div>
+            </div>
+          </div> 
         {/* Reported Postings */}
-        <div className={`${dashboardCard} cols-start-2 row-start-2`}> Reported Postings </div> 
+          <div className={`${dashboardCard} cols-start-2 row-start-2 flex flex-row justify-between`}>
+            <div className="flex flex-col">
+              <p className="font-satoshi-light text-sm"> Reported Postings </p>
+              <p className="text-5xl"> {response.reportedPostings} </p>
+            </div>
+            <div className="">
+              <div className="w-12 h-12 rounded-full bg-black flex items-center justify-center">
+                <TriangleAlert className="w-6 h-6 text-white" />
+              </div>
+            </div>
+          </div> 
         {/* Reported Users */}
-        <div className={`${dashboardCard} cols-start-1 row-start-3`}> Reported Users </div> 
+          <div className={`${dashboardCard} cols-start-1 row-start-3 flex flex-row justify-between`}>
+            <div className="flex flex-col">
+              <p className="font-satoshi-light text-sm"> Reported Users </p>
+              <p className="text-5xl"> {response.reportedUsers} </p>
+            </div>
+            <div className="">
+              <div className="w-12 h-12 rounded-full bg-black flex items-center justify-center">
+                <ShieldUser className="w-6 h-6 text-white" />
+              </div>
+            </div>
+          </div> 
         {/* Funding Requests */}
-        <div className={`${dashboardCard} cols-start-2 row-start-3`}> Funding Requests </div> 
+          <div className={`${dashboardCard} cols-start-2 row-start-3 flex flex-row justify-between`}>
+            <div className="flex flex-col">
+              <p className="font-satoshi-light text-sm"> Funding Requests </p>
+              <p className="text-5xl"> {response.fundingRequests} </p>
+            </div>
+            <div className="">
+              <div className="w-12 h-12 rounded-full bg-black flex items-center justify-center">
+                <Wallet className="w-6 h-6 text-white" />
+              </div>
+            </div>
+          </div> 
         {/* Upcoming Events */}
-        <div className={`${dashboardCard} col-span-2 row-span-2`}> Upcoming Events </div> 
-        {/* Donations */}
-        <div className={`${dashboardCard} col-span-4 row-start-4`}> Donations </div> 
+          <div className={`${dashboardCard} col-span-2 row-span-2`}> 
+            <div className="flex flex-row justify-between">
+              <p className="font-satoshi-medium text-2xl"> Upcoming Events </p> 
+                <button className="flex flex-row gap-4 items-center cursor-pointer"> <p className="font-satoshi-light">View All Events</p> <MoveRight/></button>
+            </div>
+          </div> 
+        {/* Donations Card */}
+          <div className={`${dashboardCard} col-span-4 row-span-1 flex flex-row gap-6`}> 
+          {/* Donations */}
+            <div className="flex-1 flex flex-col">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <HandCoins/>
+                  <p className="text-2xl "> Donations </p>
+                </div>
+              <button className="flex flex-row gap-4 items-center cursor-pointer"> <p className="font-satoshi-light text-sm">View All Events</p> <MoveRight/></button>            
+              </div>
+              {/* TODO: Insert percentage bars here */}
+            </div>
+            {/* Recent Donors */}
+            <div className="flex-1 flex flex-col">
+              <p className="text-2xl font-satoshi-medium"> Recent Donors</p>
+              {/* TODO: Insert recent donor table here */}
+            </div>
+          </div> 
+          <h2 className="col-span-4 text-2xl font-satoshi-medium -mb-4">More about your Alumni</h2>
         {/* Registered Alumni */}
-        <div className={`${dashboardCard} col-span-2 row-span-2`}> Registered Alumni </div> 
+          <div className={`${dashboardCard} col-span-2 row-span-2 flex flex-col`}>
+            {/* Alumni header */}
+            <div className="flex flex-row justify-between">
+              {/* Registed Alumni count */}
+              <div className="flex flex-row items-center gap-3">
+                <div className="w-12 h-12 rounded-full bg-primary flex items-center justify-center">
+                  <Users className="w-6 h-6 text-white" />
+                </div>
+                <div className="flex flex-col">
+                  <p className="font-satoshi-light -mb-1">Registered Alumni</p>
+                  <p className="font-satoshi-medium text-4xl">{response.registeredAlumni}</p>
+                </div>
+              </div>
+              {/* View Alumni Statistics */}
+              <div>
+                <button className="flex flex-row gap-4 items-center cursor-pointer"> <p className="font-satoshi-light text-sm">View All Events</p> <MoveRight/></button>      
+              </div>
+            </div>
+            {/* Alumni Locations and Industries */}
+            <div className="mt-3 flex flex-row gap-2">
+              {/* Alumni Locations */}
+              <div className="flex-1">
+                <div className="bg-secondary rounded-2xl px-4 py-1">
+                  <p className="font-satoshi-medium">Alumni Locations</p>
+                </div>
+                <div>
+                {/* TODO: Insert Countries and percent here */}
+                </div>
+              </div>
+              {/* Alumni Industries */}
+              <div className="flex-1/5">
+                <div className="bg-secondary rounded-2xl px-4 py-1">
+                  <p className="font-satoshi-medium">Alumni Industries</p>
+                </div>
+              </div>
+            </div>
+          </div> 
         {/* System Engagement */}
-        <div className={`${dashboardCard} col-span-2 row-span-2`}> System Engagement </div> 
+          <div className={`${dashboardCard} col-span-2 row-span-2 flex flex-col`}> 
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-col">
+                <h2 className="font-satoshi-medium text-2xl"> System Engagement</h2>  
+                <p className="font-satoshi-light text-xs">User visit for the last 30 days</p>
+              </div>
+              <button className="flex flex-row gap-4 cursor-pointer"> <p className="font-satoshi-light text-sm">View User Information Reports</p> <MoveRight/></button>      
+            </div>
+          </div> 
+        </div>
       </div>
     </div>
   </div>
