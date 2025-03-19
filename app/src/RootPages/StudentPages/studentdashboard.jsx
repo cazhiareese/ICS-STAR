41068067e2d6192d8ec3dce5b8c292d66bf91231
Cyrus Gello Par <cmpar@up.edu.ph>
2025-03-19 17:04:00 +0800

    chore: text adjustment for each card

--- app/src/RootPages/StudentPages/studentdashboard.jsx
+++ app/src/RootPages/StudentPages/studentdashboard.jsx
@@ -6,11 +6,11 @@ function StudentLanding() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Frontend</h1>
-      <CardComponent icon={Calendar} text="Student Profile" />
-      <CardComponent icon={Newspaper} text="Student Profile" />
-      <CardComponent icon={Briefcase} text="Student Profile" />
-      <CardComponent icon={User} text="Student Profile" />
-      <CardComponent icon={Handshake} text="Student Profile" />
+      <CardComponent icon={Calendar} text="Look for events to attend" />
+      <CardComponent icon={Newspaper} text="Catch up with ICS" />
+      <CardComponent icon={Briefcase} text="Browse job opportunities" />
+      <CardComponent icon={User} text="Connect with Alumni" />
+      <CardComponent icon={Handshake} text="Give ICS a helping hand" />
     </div>
   );
 }
