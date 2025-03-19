508192c500e4f8f800853c10e4b50f859d621442
Cyrus Gello Par <cmpar@up.edu.ph>
2025-03-19 17:02:33 +0800

    feat: student alumni cards icon access

--- app/src/RootPages/StudentPages/studentdashboard.jsx
+++ app/src/RootPages/StudentPages/studentdashboard.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { PersonStanding } from "lucide-react";
+import { Newspaper,Calendar,Briefcase, User,Handshake } from "lucide-react";
 import CardComponent from "../../components/cardcomponent";
 
 function StudentLanding() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Frontend</h1>
-      <CardComponent icon={PersonStanding} text="Student Profile" />
+      <CardComponent icon={Calendar} text="Student Profile" />
+      <CardComponent icon={Newspaper} text="Student Profile" />
+      <CardComponent icon={Briefcase} text="Student Profile" />
+      <CardComponent icon={User} text="Student Profile" />
+      <CardComponent icon={Handshake} text="Student Profile" />
     </div>
   );
 }
--- app/src/components/cardcomponent.jsx
+++ app/src/components/cardcomponent.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function CardComponent({ icon: Icon, text }) {
   return (
-    <div className="w-[184px] h-[211px] flex flex-col items-center justify-center gap-2 p-4 border rounded-lg shadow-md bg-white">
-      <span className="text-lg font-medium text-center font-satoshi-medium">{text}</span> {/* Inter font applied here */}
-      <Icon className="w-10 h-10 text-blue-500" /> {/* Icon below text */}
+    <div className="w-[184px] h-[211px] flex flex-col items-center justify-center gap-2 p-4 rounded-[20px] border border-[#D9DDEBF2] shadow-md bg-[#F9F9FB] shadow-[0px_3px_4px_0px_rgba(52,53,103,0.15)]">
+      <span className="text-lg font-medium text-center font-satoshi-medium">{text}</span> 
+      <Icon className="w-[140px] h-[139.42px] text-primary" />
     </div>
   );
 }
