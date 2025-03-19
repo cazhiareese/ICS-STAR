fa99011f2e338c94cc1446578985bfd5cf6f4b33
Cyrus Gello Par <cmpar@up.edu.ph>
2025-03-19 17:08:32 +0800

    feat: added the heading

--- app/src/RootPages/StudentPages/studentdashboard.jsx
+++ app/src/RootPages/StudentPages/studentdashboard.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Newspaper,Calendar,Briefcase, User,Handshake } from "lucide-react";
+import { Newspaper, Calendar, Briefcase, User, Handshake } from "lucide-react";
 import CardComponent from "../../components/cardcomponent";
 
 function StudentLanding() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Frontend</h1>
-      <CardComponent icon={Calendar} text="Look for events to attend" />
-      <CardComponent icon={Newspaper} text="Catch up with ICS" />
-      <CardComponent icon={Briefcase} text="Browse job opportunities" />
-      <CardComponent icon={User} text="Connect with Alumni" />
-      <CardComponent icon={Handshake} text="Give ICS a helping hand" />
+    <div className="p-6 flex flex-col items-center">
+
+      <h1 className="font-satoshi-medium font-bold text-[60px] leading-[60px] tracking-[-2%] text-center mb-8">
+        Bridging Alumni
+      </h1>
+
+
+      <h2 className="text-2xl font-bold mb-4">Frontend</h2>
+
+
+      <div className="grid grid-cols-2 gap-4">
+        <CardComponent icon={Calendar} text="Look for events to attend" />
+        <CardComponent icon={Newspaper} text="Catch up with ICS" />
+        <CardComponent icon={Briefcase} text="Browse job opportunities" />
+        <CardComponent icon={User} text="Connect with Alumni" />
+        <CardComponent icon={Handshake} text="Give ICS a helping hand" />
+      </div>
     </div>
   );
 }
