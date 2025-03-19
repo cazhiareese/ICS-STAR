45a458f63611236dfa04b4d9e1db69cf2afb0a31
Cyrus Gello Par <cmpar@up.edu.ph>
2025-03-19 17:27:10 +0800

    feat: hover effect on cards

--- app/src/components/cardcomponent.jsx
+++ app/src/components/cardcomponent.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function CardComponent({ icon: Icon, text }) {
   return (
-    <div className="w-[184px] h-[211px] flex flex-col items-center justify-center gap-2 p-4 rounded-[20px] border border-[#D9DDEBF2]  bg-[#F9F9FB] shadow-[0px_3px_4px_0px_rgba(52,53,103,0.15)]">
+    <div className="w-[184px] h-[211px] flex flex-col items-center justify-center gap-2 p-4 rounded-[20px] border border-[#D9DDEBF2] bg-[#F9F9FB] 
+                    shadow-[0px_3px_4px_0px_rgba(52,53,103,0.15)] transition-transform duration-300 hover:scale-105 hover:shadow-lg cursor-pointer">
       <span className="text-lg font-medium text-center font-satoshi-medium">{text}</span> 
       <Icon className="w-[140px] h-[139.42px] text-primary" />
     </div>
