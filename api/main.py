41a7b7468c514e7bc5058b84d5b0bdf7d584ba53
Angelo Viloria <lpviloria@up.edu.ph>
2025-03-20 00:14:14 +0800

    chore: updated main file for generating system visit reports

--- api/main.py
+++ api/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
-from routers import userlogs_routes, reports_routes
+from fastapi.middleware.cors import CORSMiddleware
+from routers import log_route
 
 app = FastAPI()
+app.include_router(log_route.router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"ICS-STAR API": "Welcome to the ICS-STAR API!"}
 
-# Include the routers from the other files
-# So far, testing has been done using FastAPI's built-in Swagger UI
-app.include_router(userlogs_routes.router)
-app.include_router(reports_routes.router)
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
