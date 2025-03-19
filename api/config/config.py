f12e4f693bf1c00b14ef233861f6dbd7a89631a8
pvgonzales <pvgonzales@up.edu.ph>
2025-03-19 16:00:48 +0800

    chore: added database connection string

--- api/config/config.py
+++ api/config/config.py
@@ -2,10 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://{}")
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "postgresql://postgres.ocmxiyulokpueycaxbuv:cmsc128@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
+engine = create_engine(DATABASE_URL, client_encoding='utf8', poolclass=NullPool)
+try:
+   connection = engine.connect()
+   print("Successfully connected to the database!")
+   connection.close()
+except Exception as e:
+   print(f"Error connecting to database: {e}")
+   
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 SECRET_KEY = "c85f7f6d5009815e7b69bf6eabc655fb1a2533d1f808fe1e950961f2b5add6da"
