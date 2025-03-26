d5182d2af4b65f3fde6ba8673fed15efdb8b216f
rfquejada <rfquejada@up.edu.ph>
2025-03-26 21:35:49 +0800

    feat: add condition in checking if user is admin

--- api/routers/admin_account_management.py
+++ api/routers/admin_account_management.py
@@ -10,6 +10,8 @@ router = APIRouter()
 # Arguments: current_user - the current user
 # Returns: True if user is an admin, False otherwise
 def isAdmin(current_user: User = Depends(require_admin)):
+    if current_user.user_type != "admin":
+        raise HTTPException(status_code=403, detail="You do not have permission to access this resource")
     return True
 
 # Get all unverified users
