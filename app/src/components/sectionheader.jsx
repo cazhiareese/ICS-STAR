e54efb0d10d031c738114640d465c36898408d30
Cyrus Gello Par <cmpar@up.edu.ph>
2025-03-26 20:02:14 +0800

    ui: updated the layout for the section header

--- app/src/components/sectionheader.jsx
+++ app/src/components/sectionheader.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function SectionHeader({ title }) {
   return (
     <div className="w-full">
-      <h2 className="text-lg font-bold text-gray-800">{title}</h2>
-      <div className="w-full border-t border-disabled mt-1"></div>
+      <h2 className="font-satoshi-bold text-[24px] leading-[22px] tracking-[-0.02em] text-gray-800 uppercase">
+        {title}
+      </h2>
+      <div className="w-full border-t border-disabled mt-2"></div> {/* Fixed border */}
     </div>
   );
 }
