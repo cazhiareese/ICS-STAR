b85e654ec5d011789cdf3c5754ad9208ff275e8b
Cyrus Gello Par <cmpar@up.edu.ph>
2025-03-26 22:15:33 +0800

    chore: created dummy skills

--- app/src/RootPages/Userprofile.jsx
+++ app/src/RootPages/Userprofile.jsx
@@ -9,7 +9,7 @@ const alumniUsers = [
   ];
   
 const signedinuser = { user_id: 2, first_name: "Jane", last_name: "Smith", mobile_number: "09234567890", age: 28, gender: "F", city: "Quezon City", state: "Metro Manila", country: "Philippines", marital_status: "Married", image: '../assets/prince boy.jpg', password: "hashed_password_here", email: "janesmith@example.com", verification_file: "verification_docs/jane_smith.pdf", user_type: "alumni", student_number: "2016-67890", graduation_year: 2020, graduation_semester: "1st Semester", employment_status: "Self-Employed", job_title: "Freelance UX Designer", work_location: "Remote", work_mode: "Remote", employer_class: "Freelancer", tenured_status: "No", salary_grade: "N/A", is_banned: false }
-  
+const skills = ["Artificial Intelligence", "Cybersecurity", "Web Development"];
 
 function UserProfile() {
     return (
