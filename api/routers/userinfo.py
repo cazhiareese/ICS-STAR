3a7f073bb6b79766c938dc62d07761545245cde7
pvgonzales <pvgonzales@up.edu.ph>
2025-03-27 11:12:10 +0800

    feat: added endpoint for adding scholarships

--- api/routers/userinfo.py
+++ api/routers/userinfo.py
@@ -1,10 +1,11 @@
-from fastapi import Depends, APIRouter, HTTPException, status, Form, UploadFile, File
+from fastapi import Depends, APIRouter, HTTPException, status, Form, UploadFile, File, Query
 from sqlalchemy.orm import Session
 from config.database import get_db
+from typing import List
 
 from util.userutil import upload_profile, get_current_user
 
-from models.usermodel import User
+from models.usermodel import User, UserScholarship, UserAffiliation
 
 router = APIRouter()
 
@@ -19,3 +20,19 @@ async def upload_profile_picture(
         raise HTTPException(status_code=500, detail="Error uploading file")
 
     return {"message": "Profile picture uploaded successfully"}
+
+@router.post("/add-scholarships")
+async def add_scholarships(
+    scholarships: List[str] = Query(...),
+    db: Session = Depends(get_db),
+    user: User = Depends(get_current_user)
+):
+    new_scholarships = [
+        UserScholarship(user_id=user.user_id, scholarship=scholarship)
+        for scholarship in scholarships
+    ]
+    
+    db.add_all(new_scholarships)
+    db.commit()
+    
+    return {"message": "scholarships added successfully"}
