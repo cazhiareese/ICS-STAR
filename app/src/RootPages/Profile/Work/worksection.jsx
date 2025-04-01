04534d037a8a598c24727b2831e4445e85718c25
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-01 21:08:22 +0800

    feat: created work section for alumni

---
+++ app/src/RootPages/Profile/Work/worksection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WorkSection() {
+  return (
+    <div className="p-6 border border-gray-300 rounded-lg bg-white">
+      <h2 className="text-2xl font-bold text-gray-900">Hello</h2>
+    </div>
+  );
+}
+
+export default WorkSection;
