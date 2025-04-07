0c4a4899bacdfcfb60787df360a4c1793841e574
ReddDL <redeleon.dev@gmail.com>
2025-04-07 10:59:19 +0800

    feat: add modal for student to alumni transition

--- app/src/RootPages/AdminPages/Records/adminuserdetails.jsx
+++ app/src/RootPages/AdminPages/Records/adminuserdetails.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
-import { MoveLeft, Check, ShieldAlert, MapPin, Phone, IdCard, GraduationCap, X  } from 'lucide-react';
+import { MoveLeft, Check, ShieldAlert, MapPin, Phone, IdCard, GraduationCap, X, CheckCircle  } from 'lucide-react';
 import axios from 'axios'
-import { div, p } from 'framer-motion/client';
 import CircularLoading from '../../../components/LoadingComponents/circularloading';
 
 function AdminUserDetails() {
@@ -11,44 +10,49 @@ function AdminUserDetails() {
 
   const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
   const [loading, setLoading] = useState(true)
+  const [limitAccessLoading, setLimitAccessLoading] = useState(false)
+  const [showAlumniModal, setShowAlumniModal] = useState(false)
+  const [makeAlumniLoading, setMakeAlumniLoading] = useState(false)
+  const [transitionComplete, setTransitionComplete] = useState(false)  
+  const [showReportsModal, setShowRepotsModal] = useState(false)
   const [reports, setReports] = useState([])
   const [user, setUser] = useState({
-    "user_id": "422aaae7-542a-4790-befa-2e2e1a6e4707",
-    "first_name": "Michael",
-    "last_name": "Brown",
-    "email": "michael.brown@example.com",
-    "mobile_number": "09090900902",
-    "age": 21,
-    "gender": "M",
-    "city": "Marinduque",
-    "state": "MIMAROPA",
-    "country": "Philippines",
-    "marital_status": "Single",
-    "image": null,
-    "user_type": "alumni",
-    "position": null,
-    "is_banned": false,
-    "student_number": "2019-23232",
-    "standing": null,
-    "graduation_year": 2024,
-    "graduation_semester": "2nd Semester",
-    "employment_status": "unemployed",
-    "industry": null,
-    "company_name": null,
-    "job_title": null,
-    "work_location": null,
-    "work_mode": null,
-    "employer_class": null,
-    "tenured_status": null,
-    "salary_grade": null,
-    "facebook": null,
-    "linkedin": null,
-    "github": null,
-    "created_at": "2025-04-06T07:16:01.008079+00:00",
-    "updated_at": "2025-04-06T09:43:01.302784+00:00",
-    "skills": [],
-    "scholarships": [],
-    "affiliations": []
+    // "user_id": ""
+    // "first_name": "",
+    // "last_name": "",
+    // "email": "",
+    // "mobile_number": "",
+    // "age": ,
+    // "gender": "M",
+    // "city": "Marinduque",
+    // "state": "MIMAROPA",
+    // "country": "Philippines",
+    // "marital_status": "Single",
+    // "image": null,
+    // "user_type": "alumni",
+    // "position": null,
+    // "is_banned": false,
+    // "student_number": "2019-23232",
+    // "standing": null,
+    // "graduation_year": 2024,
+    // "graduation_semester": "2nd Semester",
+    // "employment_status": "unemployed",
+    // "industry": null,
+    // "company_name": null,
+    // "job_title": null,
+    // "work_location": null,
+    // "work_mode": null,
+    // "employer_class": null,
+    // "tenured_status": null,
+    // "salary_grade": null,
+    // "facebook": null,
+    // "linkedin": null,
+    // "github": null,
+    // "created_at": "2025-04-06T07:16:01.008079+00:00",
+    // "updated_at": "2025-04-06T09:43:01.302784+00:00",
+    // "skills": [],
+    // "scholarships": [],
+    // "affiliations": []
   })
 
   useEffect(() => {
@@ -77,19 +81,33 @@ function AdminUserDetails() {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  function makeAlumni() {
-    alert("Made alumni")
+  async function makeAlumni() {
+    setMakeAlumniLoading(true)
+    try {
+      const response = await axios.put(`${API_BASE_URL}/admin/transition/${userid}`)
+      console.log(response)
+      setTimeout(() => {
+      }, 500)
+      setTransitionComplete(true)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setMakeAlumniLoading(false)
+    }
   }
 
-  function limitAccountAccess() {
-    alert("Limited account access")
-    axios.put(`${API_BASE_URL}/admin/ban${userid}`)
-    .then(response => {
+  async function limitAccountAccess() {
+    // alert("Limited account access")
+    setLimitAccessLoading(true)
+    try {
+      const response = await axios.put(`${API_BASE_URL}/admin/ban${userid}`)
       console.log(response)
-    }).catch(error => {
-      console.log(response)
-    })
-    setIsOpen(false)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLimitAccessLoading(false)
+      // setIsOpen(false)
+    }
   }
 
   return (
@@ -107,10 +125,15 @@ function AdminUserDetails() {
         {/* Records and confirmation button*/}
         <div className='flex lg:justify-between justify-end items-center mb-5'>
           <h1 className='text-primary font-satoshi-bold text-3xl hidden lg:block'>Records</h1>
-          <button className='flex items-center bg-success text-white text-md font-satoshi-regular gap-2 rounded-3xl px-4 py-2 cursor-pointer' onClick={() => {makeAlumni()}}>
+          {user.user_type === 'student' ? (
+          <button
+            className='flex items-center bg-success text-white text-md font-satoshi-regular gap-2 rounded-3xl px-4 py-2 cursor-pointer'
+            onClick={() => setShowAlumniModal(true)}
+          >
             <Check className='' size={20}/>
             <p> Make Alumni</p>
           </button>
+        ) : (<></>)}
         </div>
         {/* Basic information */}
         <div className='flex flex-row border border-gray-300 rounded-xl w-full p-6 mb-6'>
@@ -119,7 +142,7 @@ function AdminUserDetails() {
             <h2 className='font-satoshi-bold text-2xl'>{`${user.first_name} ${user.last_name}`}</h2>
             <p className='font-satoshi-light'>{user.email}</p>
           </div>
-          <button className='hidden lg:flex flex-row gap-2 ml-auto text-error font-satoshi-medium cursor-pointer' onClick={() => {setIsOpen(true)}}>
+          <button className='hidden lg:flex flex-row gap-2 ml-auto text-error font-satoshi-medium cursor-pointer' onClick={() => {setLimitAccessLoading(true)}}>
             <p className='hidden lg:block'>View Report Logs</p>
             <ShieldAlert/>
           </button>
@@ -190,13 +213,13 @@ function AdminUserDetails() {
           ))}
         </div>
         {/* Modal */}
-        {isOpen && (
+        {limitAccessLoading && (
           <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-50">
             <div className="flex flex-col items-center bg-white p-6 rounded-3xl shadow-lg min-w-md h-2/5">
               {/* Modal Header */}
               <div className="flex justify-between w-full items-center pb-2">
                 <h2 className="text-2xl font-satoshi-medium">Report Logs</h2>
-                <button className='rounded-full h-fit bg-error p-1 cursor-pointer' onClick={() => setIsOpen(false)}>
+                <button className='rounded-full h-fit bg-error p-1 cursor-pointer' onClick={() => limitAccessLoading(false)}>
                   <X className="w-5 h-5 text-white" />
                 </button>
               </div>
@@ -230,6 +253,58 @@ function AdminUserDetails() {
               <button className="mt-4 bg-error text-white px-4 py-2 rounded-3xl w-full cursor-pointer" onClick={() => {limitAccountAccess()}}>
                 Limit Account Access
               </button>
+            </div>
+          </div>
+        )}
+        {/* Graduation loading */}
+        {showAlumniModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-50">
+            <div className="flex flex-col justify-center items-center bg-white p-6 rounded-3xl shadow-lg w-[400px] min-h-[250px]">
+              {/* Loading Spinner */}
+              {makeAlumniLoading ? (
+                <div className='h-full'>
+                  <CircularLoading />
+                </div>
+              ) : transitionComplete ? (
+                <>
+                  <div className="text-success">
+                    <CheckCircle size={48} />
+                  </div>
+                  <p className="text-xl font-satoshi-medium mt-4 text-center">
+                    Transition to Alumni Successful!
+                  </p>
+                  <button
+                    className="bg-success text-white px-4 py-2 rounded-3xl w-full mt-6 cursor-pointer"
+                    onClick={() => {
+                      setShowAlumniModal(false)
+                      setTransitionComplete(false)
+                      window.location.reload()
+                    }}
+                  >
+                    Close
+                  </button>
+                </>
+              ) : (
+                <div className=''>
+                  <p className="text-xl font-satoshi-medium text-center mt-4">
+                    Confirm transition to Alumni?
+                  </p>
+                  <div className="flex gap-3 mt-6 w-full h-full justify-center">
+                    <button
+                      className="bg-gray-300 text-black px-4 py-2 rounded-3xl w-full cursor-pointer"
+                      onClick={() => setShowAlumniModal(false)}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      className="bg-success text-white px-4 py-2 rounded-3xl w-full cursor-pointer"
+                      onClick={makeAlumni}
+                    >
+                      Confirm
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         )}
