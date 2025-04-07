4d03db60c751695b7a5ff0a7e966679c2e942eba
ReddDL <redeleon.dev@gmail.com>
2025-04-07 08:47:46 +0800

    feat: integrate get user details

--- app/src/RootPages/AdminPages/Records/adminuserdetails.jsx
+++ app/src/RootPages/AdminPages/Records/adminuserdetails.jsx
@@ -11,11 +11,55 @@ function AdminUserDetails() {
   const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
   const [reports, setReports] = useState([])
+  const [user, setUser] = useState({
+    "user_id": "422aaae7-542a-4790-befa-2e2e1a6e4707",
+    "first_name": "Michael",
+    "last_name": "Brown",
+    "email": "michael.brown@example.com",
+    "mobile_number": "09090900902",
+    "age": 21,
+    "gender": "M",
+    "city": "Marinduque",
+    "state": "MIMAROPA",
+    "country": "Philippines",
+    "marital_status": "Single",
+    "image": null,
+    "user_type": "alumni",
+    "position": null,
+    "is_banned": false,
+    "student_number": "2019-23232",
+    "standing": null,
+    "graduation_year": 2024,
+    "graduation_semester": "2nd Semester",
+    "employment_status": "unemployed",
+    "industry": null,
+    "company_name": null,
+    "job_title": null,
+    "work_location": null,
+    "work_mode": null,
+    "employer_class": null,
+    "tenured_status": null,
+    "salary_grade": null,
+    "facebook": null,
+    "linkedin": null,
+    "github": null,
+    "created_at": "2025-04-06T07:16:01.008079+00:00",
+    "updated_at": "2025-04-06T09:43:01.302784+00:00",
+    "skills": [],
+    "scholarships": [],
+    "affiliations": []
+  })
 
   useEffect(() => {
       console.log(userid);
       const fetchData = async () => {
         try {
+          // get the user details
+          const user = await axios.get(`${API_BASE_URL}/profile/${userid}`)
+          console.log(user.data.data)
+          setUser(user.data.data)
+
+          // get the reports
           const reports = await axios.get(`${API_BASE_URL}/admin/report-logs/${userid}`)
           console.log(reports.data)
           setReports(reports.data)
@@ -35,25 +79,15 @@ function AdminUserDetails() {
 
   function limitAccountAccess() {
     alert("Limited account access")
+    axios.put(`${API_BASE_URL}/admin/ban${userid}`)
+    .then(response => {
+      console.log(response)
+    }).catch(error => {
+      console.log(response)
+    })
     setIsOpen(false)
   }
 
-  const skills = ['Artificial Intelligence', 'Cybersecurity', 'Web Development']
-  const affiliations = [
-    {
-      "organization": "Young Software Engineers' Society",
-      "role": "Resident Member"
-    },
-    {
-      "organization": "Young Software Engineers' Society",
-      "role": "Resident Member"
-    },
-    {
-      "organization": "Young Software Engineers' Society",
-      "role": "Resident Member"
-    }
-  ]
-  const scholarships = ["DOST Scholarship", "UPLB SLAS"]
   return (
     <div className='p-6 overflow-auto max-h-screen'>
       {/* Back */}
@@ -73,8 +107,8 @@ function AdminUserDetails() {
       <div className='flex flex-row border border-gray-300 rounded-xl w-full p-6 mb-6'>
         <div className='border border-black rounded-full h-30 w-30'></div>
         <div className='flex flex-col justify-center ml-8'>
-          <h2 className='font-satoshi-bold text-2xl'>Kiefer Tayawa</h2>
-          <p className='font-satoshi-light'>kltayawa@up.edu.ph</p>
+          <h2 className='font-satoshi-bold text-2xl'>{`${user.first_name} ${user.last_name}`}</h2>
+          <p className='font-satoshi-light'>{user.email}</p>
         </div>
         <button className='hidden lg:flex flex-row gap-2 ml-auto text-error font-satoshi-medium cursor-pointer' onClick={() => {setIsOpen(true)}}>
           <p className='hidden lg:block'>View Report Logs</p>
@@ -91,7 +125,7 @@ function AdminUserDetails() {
             <MapPin/>
             <h3 className='font-satoshi-light ml-1'>Location</h3>
           </div>
-          <h3 className='font-satoshi-medium ml-7'> Manila, PH</h3>
+          <h3 className='font-satoshi-medium ml-7'> {user.work_location}</h3>
         </div>
         {/* Mobile Number */}
         <div className='flex flex-col'>
@@ -99,7 +133,7 @@ function AdminUserDetails() {
             <Phone/>
             <h3 className='font-satoshi-light ml-1'>Mobile Number</h3>
           </div>
-          <h3 className='font-satoshi-medium ml-7'> 09123456789</h3>
+          <h3 className='font-satoshi-medium ml-7'> {user.mobile_number} </h3>
         </div>
         {/* Student Number */}
         <div className='flex flex-col'>
@@ -107,7 +141,7 @@ function AdminUserDetails() {
             <IdCard/>
             <h3 className='font-satoshi-light ml-1'>Student Number</h3>
           </div>
-          <h3 className='font-satoshi-medium ml-7'> 1234-56789 </h3>
+          <h3 className='font-satoshi-medium ml-7'> {user.student_number} </h3>
         </div>
         {/* Graduating Class */}
         <div className='flex flex-col'>
@@ -115,22 +149,22 @@ function AdminUserDetails() {
             <GraduationCap/>
             <h3 className='font-satoshi-light ml-1'>Graduating Class</h3>
           </div>
-          <h3 className='font-satoshi-medium ml-7'> 2022 - 1st Semester</h3>
+          <h3 className='font-satoshi-medium ml-7'> {`${user.graduation_year} ${user.graduation_semester}`} </h3>
         </div>
       </div>
       {/* SKILLS AND INTERESTS */}
       <h2 className='font-satoshi-medium text-xl'> SKILLS AND INTERESTS</h2>
       <div className='border-t border-gray-300 flex flex-row flex-wrap gap-3 py-5 mb-5'>
-        {skills.map((skill, index) => (
+        {user.skills.map((skill, index) => (
           <div key={index} className="rounded-4xl p-2 border-2 w-fit border-black">
             <p className='font-satoshi-medium lg:text-md text-sm whitespace-nowrap'> {skill} </p>
           </div>
         ))}
       </div>
       {/* AFFILIATIONS */}
-      <h2 className='font-satoshi-medium text-xl'> SKILLS AND INTERESTS</h2>
+      <h2 className='font-satoshi-medium text-xl'> AFFILIATIONS </h2>
       <div className='border-t border-gray-300 grid grid-cols-2 w-full gap-3 py-5 mb-5'>
-        {affiliations.map((affiliation, index) => (
+        {user.affiliations.map((affiliation, index) => (
           <div key={index} className="">
             <p className='font-satoshi-medium'> {affiliation.organization} </p>
             <p className='font-satoshi-light'> {affiliation.role}</p>
@@ -140,7 +174,7 @@ function AdminUserDetails() {
       {/* SCHOLARSHIPS */}
       <h2 className='font-satoshi-medium text-xl'> SCHOLARSHIPS </h2>
       <div className='border-t border-gray-300 grid grid-cols-2 w-full gap-3 py-5 mb-5'>
-        {scholarships.map((scholarship, index) => (
+        {user.scholarships.map((scholarship, index) => (
           <div key={index} className="">
             <p className='font-satoshi-medium'> {scholarship} </p>
           </div>
