ccb564b9a62786e661e46821020caa76d16f11e0
pvgonzales <pvgonzales@up.edu.ph>
2025-04-09 00:56:44 +0800

    fix: changed the value of user_type in creating tokens

--- api/util/userutil.py
+++ api/util/userutil.py
@@ -215,7 +215,7 @@ async def register_user(
     
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": str(new_user.user_id), "role": str(new_user.user_type)}, expires_delta=access_token_expires
+        data={"sub": str(new_user.user_id), "role": new_user.user_type.value}, expires_delta=access_token_expires
     )
 
     return {"message": "Account created successfully", "access_token": access_token}
