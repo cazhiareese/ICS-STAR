12c67bb60bcc990dd141e37f80f6adaeb22477bb
pvgonzales <pvgonzales@up.edu.ph>
2025-04-09 18:00:32 +0800

    feat: created a util that fetches general donation drive details

--- api/util/alum_donation_util.py
+++ api/util/alum_donation_util.py
@@ -56,3 +56,30 @@ def get_one_donation_drive(db: Session, drive: DonationDrive) -> OneDonationDriv
         link=link_list,
         created_at=drive.created_at
     )
+    
+def general_donation_drive(db: Session, drive: DonationDrive) -> OneDonationDriveOut:
+    monetary_data = db.query(
+        func.coalesce(func.sum(MonetaryDonation.amount), 0).label('total_amount_donated'),
+        func.count(MonetaryDonation.donation_id).label('monetary_count')
+    ).filter(MonetaryDonation.drive_id == drive.drive_id).one()
+
+    total_amount_donated = monetary_data.total_amount_donated    
+    monetary_count = monetary_data.monetary_count
+    in_kind_count = db.query(func.count(InKindDonation.donation_id)).filter(InKindDonation.drive_id == drive.drive_id).scalar()
+
+    donation_count = monetary_count + in_kind_count
+    
+    links = db.query(DonationDriveLink.link).filter(DonationDriveLink.drive_id == drive.drive_id).all()
+    link_list = [link[0] for link in links] if links else None
+
+    return OneDonationDriveOut(
+        title=drive.title,
+        description=drive.description,
+        target_cost=float(drive.target_cost or 0),
+        image_url=drive.image,
+        total_amount_donated=float(total_amount_donated or 0),
+        in_kind_count=in_kind_count,
+        donation_count=donation_count,
+        link=link_list,
+        created_at=drive.created_at
+    )
