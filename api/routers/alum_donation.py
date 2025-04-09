cd063d93c885ae918b6ae55ac1b3dcaaee527d39
pvgonzales <pvgonzales@up.edu.ph>
2025-04-09 18:00:04 +0800

    feat: created an endpoint to fetch the general donation drive

--- api/routers/alum_donation.py
+++ api/routers/alum_donation.py
@@ -5,7 +5,7 @@ from typing import List
 from models.donationmodel import DonationDrive, MonetaryDonation, InKindDonation, DonationDriveLink
 from schemas.donation_schema import DonationDriveOut, OneDonationDriveOut
 from config.database import get_db
-from util.alum_donation_util import get_donation_drive_data, get_one_donation_drive
+from util.alum_donation_util import get_donation_drive_data, get_one_donation_drive, general_donation_drive
 from util.userutil import get_current_user
 from models.usermodel import User
 from schemas.user import UserTypeEnum
@@ -68,3 +68,24 @@ def get_one_drive(
         raise HTTPException(status_code=404, detail="Donation drive not found")
     
     return get_one_donation_drive(db, drive)
+
+@router.get("/gen-donation-drive", response_model=OneDonationDriveOut)
+def get_one_drive(
+    db: Session = Depends(get_db), 
+    user: User = Depends(get_current_user)
+):
+    if user.user_type.value == UserTypeEnum.student:
+        raise HTTPException(status_code=400, detail="For alumni only")
+    
+    drive = db.query(DonationDrive).filter(
+        and_(
+            DonationDrive.is_general == True,
+            DonationDrive.is_deleted == False,
+            DonationDrive.is_closed == False
+        )
+    ).first()
+    
+    if not drive:
+        raise HTTPException(status_code=404, detail="Donation drive not found")
+    
+    return general_donation_drive(db, drive)
