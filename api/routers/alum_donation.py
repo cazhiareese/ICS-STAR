55003c765b1ba1ccd349136f5d8dfb2386b97d71
pvgonzales <pvgonzales@up.edu.ph>
2025-04-09 14:44:46 +0800

    fix: bug check

--- api/routers/alum_donation.py
+++ api/routers/alum_donation.py
@@ -28,3 +28,20 @@ def get_donation_drives(db: Session = Depends(get_db), user: User = Depends(get_
     donation_data = [get_donation_drive_data(db, drive) for drive in drives]
 
     return donation_data
+
+@router.get("/donationdrive-limit", response_model=List[DonationDriveOut])
+def get_donation_drives(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+    
+    if user.user_type.value == UserTypeEnum.student:
+        raise HTTPException(status_code=400, detail="For alumni only")
+    
+    drives = db.query(DonationDrive).filter(
+        and_(
+            DonationDrive.is_deleted == False,
+            DonationDrive.is_closed == False
+        )
+    ).limit(5).all()
+    
+    donation_data = [get_donation_drive_data(db, drive) for drive in drives]
+
+    return donation_data
