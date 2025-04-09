2b9413dd2d53fde1773d8db0138054962cc61ade
ReddDL <redeleon.dev@gmail.com>
2025-04-10 00:29:03 +0800

    ui: add conditional rendering for open donation drives

--- app/src/RootPages/AdminPages/Donations/admindonationinformation.jsx
+++ app/src/RootPages/AdminPages/Donations/admindonationinformation.jsx
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import { MoveLeft } from 'lucide-react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 import VerifiedDonationsTable from "../../../components/AdminComponents/verifieddonationstable"
 import PendingDonationsTable from '../../../components/AdminComponents/pendingdonationstable';
+import axios from 'axios';
 
 function AdminDonationInformation() {
   const navigate = useNavigate()
+  const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+  const {driveid} = useParams();
 
   const [progressData, setProgressData] = useState([
     { name: "progress", value: 20 },
@@ -15,92 +18,96 @@ function AdminDonationInformation() {
 
   const [pendingDonations, setPendingDonations] = useState([])
   const [verifiedDonations, setverifiedDonations] = useState([])
+  const [isClosed, setIsClosed] = useState(true)
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
+    //  const response = await axios.get(`${API_BASE_URL}/admin/donations/${driveid}`)
+    //  console.log(response.data)   
+
         setProgressData([
           { name: "progress", value: 20 },
           { name: "remaining", value: 80 },
         ]);
         setPendingDonations([
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "₱1,000.00" },
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "Donation details here..." },
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "Donation details here..." },
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "₱1,000.00" },
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "₱1,000.00" },
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "₱1,000.00" },
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "₱1,000.00" },
-          { date: "1/23/25 1:00 PM", donor: "Lorem Ipsum", details: "Donation details here..." },
+          {
+            "donation_id": "f2518831-8819-458d-aaa4-b0680ba3f5c2",
+            "name": "Benjamin Ramirez",
+            "donation_details": "₱8912.51",
+            "date_donated": "2025-04-02T13:50:59.152511Z"
+          },
+          {
+            "donation_id": "8c860069-21b4-4e95-895e-d6c98bb42e59",
+            "name": "Charliee Garcia",
+            "donation_details": "₱5000.00",
+            "date_donated": "2025-04-09T13:38:36.449972Z"
+          }
         ]);
         setverifiedDonations([
           {
-            "date_donated": "2025-04-01",
-            "name": "Alice Reyes",
-            "donation_type": "Cash",
-            "donation_details": 5000
+            "donation_id": "3cf6a1a3-933c-4642-8f46-7a73c8344107",
+            "date_donated": "2025-04-01T13:50:59.152176Z",
+            "name": "Juan Lleva",
+            "donation_type": "Monetary",
+            "donation_details": "₱8,058.72"
           },
           {
-            "date_donated": "2025-04-03",
-            "name": "Carlos Dela Cruz",
-            "donation_type": "Cash",
-            "donation_details": 2000
+            "donation_id": "a58e857a-71e9-435c-8c70-d576b0aa33fb",
+            "date_donated": "2025-03-13T13:50:59.152808Z",
+            "name": "Lucas Fernandez",
+            "donation_type": "Monetary",
+            "donation_details": "₱5,716.75"
           },
           {
-            "date_donated": "2025-04-05",
-            "name": "Bea Santos",
-            "donation_type": "Cash",
-            "donation_details": 4500
+            "donation_id": "537f62bc-1b96-4352-a13e-294a6636627e",
+            "date_donated": "2025-03-18T13:50:59.152931Z",
+            "name": "porting totoy",
+            "donation_type": "Monetary",
+            "donation_details": "₱9,866.51"
           },
           {
-            "date_donated": "2025-04-07",
-            "name": "Daniel Ong",
-            "donation_type": "Cash",
-            "donation_details": 1000
+            "donation_id": "544609dc-1951-4e81-b4db-645aebbe07dc",
+            "date_donated": "2025-03-12T13:50:59.153022Z",
+            "name": "Ethan Cruz",
+            "donation_type": "Monetary",
+            "donation_details": "₱3,190.92"
           },
           {
-            "date_donated": "2025-04-08",
-            "name": "Janelle Lim",
-            "donation_type": "Cash",
-            "donation_details": 1500
+            "donation_id": "a5993dc0-9e82-4e04-8829-6ec04ea3b9f6",
+            "date_donated": "2025-03-18T13:50:59.152931Z",
+            "name": "porting totoy",
+            "donation_type": "In-kind",
+            "donation_details": "Boxes of canned goods and clothes"
           },
           {
-            "date_donated": "2025-04-08",
-            "name": "Janelle Lim",
-            "donation_type": "Cash",
-            "donation_details": 1500
+            "donation_id": "bfa9896e-f188-48df-9c60-c29129ce2e53",
+            "date_donated": "2025-04-01T13:50:59.152176Z",
+            "name": "Juan Lleva",
+            "donation_type": "In-kind",
+            "donation_details": "Boxes of canned goods and clothes"
           },
           {
-            "date_donated": "2025-04-08",
-            "name": "Janelle Lim",
-            "donation_type": "Cash",
-            "donation_details": 1500
+            "donation_id": "ecd4a8d5-81b7-43d3-ac08-3e37e307ea3f",
+            "date_donated": "2025-03-13T13:50:59.152808Z",
+            "name": "Lucas Fernandez",
+            "donation_type": "In-kind",
+            "donation_details": "Boxes of canned goods and clothes"
           },
           {
-            "date_donated": "2025-04-08",
-            "name": "Janelle Lim",
-            "donation_type": "Cash",
-            "donation_details": 1500
+            "donation_id": "bdabd07b-9801-4f15-afe2-3ef0d2d5061c",
+            "date_donated": "2025-03-12T13:50:59.153022Z",
+            "name": "Ethan Cruz",
+            "donation_type": "In-kind",
+            "donation_details": "Boxes of canned goods and clothes"
           },
           {
-            "date_donated": "2025-04-08",
-            "name": "Janelle Lim",
-            "donation_type": "Cash",
-            "donation_details": 1500
-          },
-          {
-            "date_donated": "2025-04-08",
-            "name": "Janelle Lim",
-            "donation_type": "Cash",
-            "donation_details": 1500
-          },
-          {
-            "date_donated": "2025-04-08",
-            "name": "Janelle Lim",
-            "donation_type": "Cash",
-            "donation_details": 1500
-          },
-        ]
-        )
+            "donation_id": "f40902d6-c7ca-4a7d-b046-57f12179637f",
+            "date_donated": "2025-04-02T13:50:59.152511Z",
+            "name": "Benjamin Ramirez",
+            "donation_type": "In-kind",
+            "donation_details": "Boxes of canned goods and clothes"
+          }
+        ])
     };
 
     fetchData();
@@ -119,11 +126,35 @@ function AdminDonationInformation() {
       </button>
       {/* Donation name and tag */}
       <div className='flex flex-row justify-between mb-5'>
-        <h1 className='font-satoshi-bold text-primary text-4xl'>ICS New Aircon</h1>
+        <div className='flex flex-row gap-2'>
+          <h1 className='font-satoshi-bold text-primary text-4xl'>ICS New Aircon</h1>
+          {/* Open or closed button */}
+          {isClosed ? (
+            <div className='bg-green-100 px-7 py-1 rounded-3xl flex items-center h-fit place-self-end'> 
+            <p className='text-success font-satoshi-medium text-sm'>Open</p>
+          </div>          ) : (
+            <div className='bg-green-100 px-7 py-1 rounded-3xl flex items-center h-fit place-self-end'> 
+              <p className='text-success font-satoshi-medium text-sm'>Open</p>
+            </div>
+          )}
+        </div>
         {/* Generate Report or close drive */}
-        <button className='bg-error text-white px-7 shadow-lg rounded-2xl'>
-          <p className='font-satoshi-light'>Close Drive</p>
-        </button>
+        <div className='h-full gap-5 flex flex-row'>
+          {isClosed ? (
+            <></>
+          ) : (
+            <>
+              {/* View Details */}
+              <button className='bg-primary text-white px-7 py-2 shadow-lg rounded-2xl hover:bg-hover cursor-pointer'>
+                <p className='font-satoshi-light'>View Details</p>
+              </button>
+              {/* Close Drive */}
+              <button className='bg-error text-white px-7 py-2 shadow-lg rounded-2xl cursor-pointer'>
+                <p className='font-satoshi-light'>Close Drive</p>
+              </button>
+            </>
+          )}
+        </div>
       </div>
       {/* Goal progress and recent transactions */}
       <div className='flex flex-row gap-2 h-1/3 mb-9'>
