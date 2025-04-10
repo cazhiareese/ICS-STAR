3ad79a0cb2c746e5b73e3637104f8f7d72fc2e5c
ReddDL <redeleon.dev@gmail.com>
2025-04-10 15:15:02 +0800

    feat: created expanded pending donations page

--- app/src/RootPages/AdminPages/Donations/adminpendingdonations.jsx
+++ app/src/RootPages/AdminPages/Donations/adminpendingdonations.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useParams } from 'react-router-dom'
+import { MoveLeft } from 'lucide-react'
+import ExpandedPendingDonations from '../../../components/AdminComponents/expandedpendingdonations'
 
 function AdminPendingDonations() {
   const {driveid} = useParams()
+  const [pendingDonations, setPendingDonations] = useState([
+    {
+      "date_time_donated": "2025-04-21T16:30:00",
+      "donor": "John Doe",
+      "donation_type": "In-kind",
+      "donation_amount": "Solar Panel"
+    },
+    {
+      "date_donated": "2025-04-09T11:41:00",
+      "name": "Juan Dela Cruz",
+      "donation_type": "Monetary",
+      "donation_amount": "₱5,000"
+    }
+  ])
 
   return (
     <div className='flex flex-col lg:p-6 h-screen overflow-hidden max-w-7xl mx-auto'>
-      PendingDonations
-      {driveid}
-    </div>
+      <button className="flex gap-2 mb-3 flex-row items-center cursor-pointer" onClick={() => navigate(-1)}>
+        <MoveLeft className='text-primary'/> 
+        <p className='text-primary font-satoshi-medium text-lg'>Back to Donations List</p>
+      </button>
+      <h1 className='text-primary text-5xl font-satoshi-bold'>New ICS Aircon</h1>
+      <h2 className='text-black text-3xl font-satoshi-medium'>Pending Verifications</h2>
+      <div className='border border-gray-300 rounded-2xl h-full overflow-auto'>
+        <ExpandedPendingDonations data={pendingDonations}/>
+      </div>
+    </div> 
   )
 }
 
---
+++ app/src/components/AdminComponents/expandedpendingdonations.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function ExpandedPendingDonations({ data }) {
+
+  return (
+    <table className="w-full ">
+        {/* Table Header */}
+        <thead>
+            <tr className="text-left text-xs text-primary font-satoshi-regular">
+            <th className="py-2 px-4">Date and time</th>
+            <th className="py-2 px-4">Donor</th>
+            <th className="py-2 px-4">Donation Type</th>
+            <th className="py-2 px-4">Amount/Description</th>
+            <th className="py-2 px-4">Verify</th>
+            </tr>
+        </thead>
+
+        {/* Table Body */}
+        <tbody className="font-satoshi-regular text-sm">
+            {data.map((donation, index) => (
+                <tr
+                key={index}
+                className="hover:bg-gray-100 cursor-pointer"
+                // onClick={() => navigate(`/admin/donations/${donation.donation_id}`)}
+                >
+                <td className="py-3 px-4 flex items-center">{donation.date_donated}</td>
+                <td className="py-3 px-4">{donation.name}</td>
+                <td className="py-3 px-4">{donation.donation_type}</td>
+                <td className="py-3 px-4">{donation.donation_amount}</td>
+                <td className="py-3 px-4">
+                    <button className='w-full h-full py-1 rounded-2xl bg-primary cursor-pointer hover:bg-hover'>
+                        <p className='font-satoshi-regular text-white text-lg'>Review</p>
+                    </button>
+                </td>
+            </tr>
+            ))}
+        </tbody>
+    </table>
+  )
+}
+
+export default ExpandedPendingDonations
