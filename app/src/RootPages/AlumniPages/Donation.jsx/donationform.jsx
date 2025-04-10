914748be6042b726f186199df4c845541c77f2d5
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-10 16:08:24 +0800

    fix: duplicates in donation form

--- app/src/RootPages/AlumniPages/Donation.jsx/donationform.jsx
+++ app/src/RootPages/AlumniPages/Donation.jsx/donationform.jsx
@@ -36,9 +36,6 @@ function Donationform() {
         const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
         return new Date(date).toLocaleDateString(undefined, options);
     };
-
-    const [donationDetailsInput, setDonationDetailsInput] = useState("");
-    const [donationSuccess, setDonationSuccess] = useState(false);
     const handleFileSubmit = (file) => {
         setFile(file);
     };
