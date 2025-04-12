120adf8f26ad757e2e497d1f7fcf6a11751b62dd
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-12 11:09:45 +0800

    chore: reverted the context

--- app/src/AuthPages/AuthContext/onboardingcontext.jsx
+++ app/src/AuthPages/AuthContext/onboardingcontext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-//const OnboardingContext = createContext();      please fix this line
+const OnboardingContext = createContext();      
 
 export function OnboardingProvider({ children }) {
     const [currentSection, setCurrentSection] = useState(0);
@@ -59,11 +59,11 @@ export function OnboardingProvider({ children }) {
     );
 }
 
-// Ensure function is wrapped properly
-// export function useOnboardingContext() {           please fix this function
-//     const context = useContext(OnboardingContext);
-//     if (!context) {
-//         throw new Error("useOnboardingContext must be used within an OnboardingProvider");
-//     }
-//     return context;
-// }
+//Ensure function is wrapped properly
+export function useOnboardingContext() {           
+    const context = useContext(OnboardingContext);
+    if (!context) {
+        throw new Error("useOnboardingContext must be used within an OnboardingProvider");
+    }
+    return context;
+}
