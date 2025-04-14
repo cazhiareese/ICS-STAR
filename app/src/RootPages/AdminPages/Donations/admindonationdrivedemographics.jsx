1ee9188a577c43191645ae2869f3ba27c8a9f795
ReddDL <redeleon.dev@gmail.com>
2025-04-15 00:36:02 +0800

    feat: add donor batch breakdown

--- app/src/RootPages/AdminPages/Donations/admindonationdrivedemographics.jsx
+++ app/src/RootPages/AdminPages/Donations/admindonationdrivedemographics.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {MoveLeft} from 'lucide-react'
+import { PieChart, ResponsiveContainer, Pie, Cell, Tooltip } from 'recharts'
 
 function AdminDonationDriveDemographics() {
+  const navigate = useNavigate()
+  const [donorsByBatch, setDonorsByBatch] = useState([])
+  
+  useEffect(() => {
+    setDonorsByBatch([
+      { batch: "2022", donors: 60 },
+      { batch: "2010", donors: 20 },
+      { batch: "2011", donors: 10 },
+      { batch: "Others", donors: 10 }
+    ])
+  }, [])
+  
+  const COLORS = ['#0a3d91', '#5a78c8', '#a3b9ec', '#d8e4fa'];
+
   return (
-    <div>
-        Admin Donation Drive Demographics
+    <div className='p-6 flex flex-col h-screen'>
+      <button className="flex gap-2 mb-3 flex-row items-center cursor-pointer" onClick={() => navigate(-1)}>
+        <MoveLeft className='text-primary'/> 
+        <p className='text-primary font-satoshi-medium text-lg'>Back to Donations List</p>
+      </button>
+      <h1 className='text-4xl font-satoshi-bold mb-3'>Donor Demographics</h1>
+      <div className='flex flex-row border border-gray-300 rounded-2xl p-4 h-1/3 w-full'>
+        <div className='flex-1 h-full w-full flex flex-col'>
+          <h2 className='font-satoshi-medium text-lg '>Donors' Batch Breakdown</h2>
+          <div className='h-full w-full flex flex-row'>
+            <div className='flex-1'>
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={donorsByBatch}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius="80%"
+                    dataKey="donors"
+                    stroke="none"
+                    >
+                    {donorsByBatch.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                    ))}
+                  </Pie>
+                  <Tooltip
+                    formatter={(value, name, props) => [`${value} Donors`, `Batch ${props.payload.batch}`]}
+                    cursor={{ fill: 'rgba(0, 0, 0, 0.1)' }}
+                  />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+            <div className='flex justify-center flex-col'>
+              {donorsByBatch.map((entry, index) => (
+                <div key={index} className="flex items-center gap-2 text-sm font-satoshi-regular">
+                  <div className="w-4 h-4" style={{ backgroundColor: COLORS[index] }} />
+                  <p className="">
+                      Batch {entry.batch}: {entry.donors} Donors
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className='border-l border-gray-300 mx-4'></div>
+        <div className='flex-1'>
+
+        </div>
+      </div>
     </div>
   )
 }
