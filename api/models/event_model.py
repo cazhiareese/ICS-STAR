681cea712105681e5ecae60c9ca4fea43c2d9326
pvgonzales <pvgonzales@up.edu.ph>
2025-04-20 14:19:32 +0800

    feat: added models for event's multivalued attributes

--- api/models/event_model.py
+++ api/models/event_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Boolean, Numeric, Text, ForeignKey, DateTime, func, UUID
+from sqlalchemy import Column, Boolean, Text, ForeignKey, DateTime, Date, func, UUID
 from sqlalchemy.orm import relationship
 import uuid
 from config.config import Base
@@ -19,6 +19,9 @@ class Event(Base):
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     confirmed_by = relationship("EventConfirmedBy", foreign_keys="[EventConfirmedBy.event_id]", back_populates="event")
     dates = relationship("EventDate", foreign_keys="[EventDate.event_id]", back_populates="event")
+    links = relationship("EventLink", back_populates="event")
+    tags = relationship("EventTag", back_populates="event")
+    visible_to_users = relationship("EventVisibleTo", back_populates="event")
 
 class EventConfirmedBy(Base):
     __tablename__ = "event_confirmed_by"
@@ -40,3 +43,33 @@ class EventDate(Base):
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
     event = relationship("Event", foreign_keys=[event_id], back_populates="dates")
+
+class EventLink(Base):
+    __tablename__ = 'event_link'
+
+    event_id = Column(UUID(as_uuid=True), ForeignKey('event.event_id'), primary_key=True)
+    link = Column(Text, primary_key=True)
+    created_at = Column('created_at', DateTime(timezone=True), server_default=func.now())
+    updated_at = Column('updated_at', DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    event = relationship("Event", back_populates="links")
+
+class EventTag(Base):
+    __tablename__ = 'event_tag'
+
+    event_id = Column(UUID(as_uuid=True), ForeignKey('event.event_id'), primary_key=True)
+    tag = Column(Text, primary_key=True)
+    created_at = Column('created_at', DateTime(timezone=True), server_default=func.now())
+    updated_at = Column('updated_at', DateTime(timezone=True),  server_default=func.now(), onupdate=func.now())
+
+    event = relationship("Event", back_populates="tags")
+
+class EventVisibleTo(Base):
+    __tablename__ = 'event_visible_to'
+
+    event_id = Column(UUID(as_uuid=True), ForeignKey('event.event_id'), primary_key=True)
+    user_id = Column(UUID(as_uuid=True), primary_key=True)
+    created_at = Column('created_at', DateTime(timezone=True), server_default=func.now())
+    updated_at = Column('updated_at', DateTime(timezone=True),server_default=func.now(), onupdate=func.now())
+
+    event = relationship("Event", back_populates="visible_to_users")
