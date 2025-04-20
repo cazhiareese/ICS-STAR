0f3fa7e68703ba6e053021e07e4c5a6ce022aa20
pvgonzales <pvgonzales@up.edu.ph>
2025-04-20 14:20:00 +0800

    feat: added schema for event details

--- api/schemas/events_schema.py
+++ api/schemas/events_schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
+from typing import List
 
 class EventOut(BaseModel):
     event_id: UUID
@@ -8,6 +9,19 @@ class EventOut(BaseModel):
     description: str
     location: str
     event_datetime: datetime
+
+    class Config:
+        from_attributes = True
+
+class OneEventOut(BaseModel):
+    event_id: UUID
+    title: str
+    description: str
+    image: str
+    location: str
+    datetimes: List[datetime]
+    links: List[str]
+    tags: List[str]
 
     class Config:
         from_attributes = True
