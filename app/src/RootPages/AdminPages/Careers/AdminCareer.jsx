ababe4638fb0cbfd2a5910bb1f69555447c904ad
ReddDL <redeleon.dev@gmail.com>
2025-04-22 00:08:26 +0800

    feat: can view open, closed, reported, and top job postings

--- app/src/RootPages/AdminPages/Careers/AdminCareer.jsx
+++ app/src/RootPages/AdminPages/Careers/AdminCareer.jsx
@@ -3,79 +3,118 @@ import AdminCareerCard from '../../../components/AdminComponents/admincareercard
 import { ChevronLeft, ChevronRight, Filter, List, LayoutGrid, MoveRight, MoveLeft } from 'lucide-react'
 import JobTable from '../../../components/AdminComponents/JobTable';
 import AdminModal from '../../../components/AdminComponents/adminmodal';
-
-const jobPostings = [
-  {
-    job_title: 'Software Engineer',
-    org: 'Institute of Computer Science, UPLB',
-    location: 'Los Baños, Laguna',
-    interested: 128,
-    date: 'January 1, 2025',
-    creator: 'John Doe',
-  },
-  {
-    title: 'Frontend Developer',
-    org: 'Tech Inc.',
-    location: 'Taguig, Metro Manila',
-    interested: 90,
-    date_posted: 'February 5, 2025',
-    poster: 'Jane Smith',
-  },
-  {
-    title: 'Backend Engineer',
-    org: 'Code Lab',
-    location: 'Quezon City',
-    interested: 76,
-    date: 'March 3, 2025',
-    poster: 'Mark Dela Cruz',
-  },
-];
+import axios from 'axios'
+import CircularLoading from "../../../components/LoadingComponents/circularloading"
 
 function AdminCareer() {
+
+  const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
+
   const [index, setIndex] = useState(0);
-  const total = jobPostings.length;
   const [viewStyle, setViewStyle] = useState('List')
   const [jobType, setJobType] = useState('open')
   const [page, setPage] = useState()
   const [totalPages, setTotalPages] = useState()
   const [jobs, setJobs] = useState([]);
   const [selectedJob, setSelectedJob] = useState(null);
+  const [topJobs, setTopJobs] = useState([])
+  const [topLoading, setTopLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
 
+  const [openCount, setOpenCount] = useState(0)
+  const [closedCount, setClosedCount] = useState(0)
+  const [reportedCount, setReportedCount] = useState(0)
+  
+  const total = topJobs.length;
   const prev = () => setIndex((index - 1 + total) % total);
   const next = () => setIndex((index + 1) % total);
 
-  useEffect(() => {
-    const fetchJobs = () => {
-      setJobs([
-        {
-          date_posted: '01/25/2025',
-          job_title: 'Software Developer',
-          creator: 'John Doe',
-          interested: 25,
-        },
-        {
-          date_posted: '01/25/2025',
-          job_title: 'Software Developer',
-          creator: 'John Doe',
-          interested: 15,
-        },
-      ]);
-    };
+  async function fetchJobs(type) {
+    let endpoint = '';
+    if (type === 'open') {
+      endpoint = `${API_BASE_URL}/admin/job-postings/open`;
+    } else if (type === 'closed') {
+      endpoint = `${API_BASE_URL}/admin/job-postings/closed`;
+    } else if (type === 'reported') {
+      endpoint = `${API_BASE_URL}/admin/job-postings/reported`;
+    }
+  
+    try {
+      const response = await axios.get(endpoint);
+      console.log(response)
+      setJobs(response.data);
+    } catch (error) {
+      console.error('Failed to fetch jobs:', error);
+    }
+  }
+``
 
-    fetchJobs();
-  }, []);
+  async function fetchJobCounts() {
+    const openResponse = await axios.get(`${API_BASE_URL}/admin/job-postings/open/count`)
+    console.log(openResponse.data)
+    setOpenCount(openResponse.data.open_job_postings_count)
+
+    const closedResponse = await axios.get(`${API_BASE_URL}/admin/job-postings/closed/count`)
+    console.log(closedResponse.data)
+    setClosedCount(closedResponse.data.closed_job_postings_count)
+
+    const reportedResponse = await axios.get(`${API_BASE_URL}/admin/job-postings/reported/count`)
+    console.log(reportedResponse.data)
+    setReportedCount(reportedResponse.data.reported_job_postings_count)
+  }
+
+  useEffect(() => {
+    async function fetchTopJobs() {
+      setTopLoading(true)
+      try {
+        const response = await axios.get(`${API_BASE_URL}/admin/job-postings/top-4-interested`)
+        console.log(response)
+        setTopJobs(response.data)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setTopLoading(false)
+      }
+    }
+    fetchTopJobs()
+  }, [])
+  
+  useEffect(() => {
+    async function fetchAllJobs () {
+      setLoading(true);
+      try {
+        await fetchJobCounts()
+        await fetchJobs(jobType);
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    fetchAllJobs();
+  }, [jobType]);
+  
 
   return (
     <div className='flex flex-col h-screen p-6 items-center w-full'>
       <h1 className='text-primary font-satoshi-bold text-5xl mb-4 self-start'>Career</h1>
       {/* Card Carousel */}
-      <div className='flex items-center gap-4 w-full justify-center' onClick={() => setSelectedJob(jobPostings[index])}>
-      <AdminCareerCard job={jobPostings[index]} onPrev={prev} onNext={next} />
-      </div>
+      {topLoading ? (
+        <CircularLoading />
+      ) : topJobs.length > 0 ? (
+        <div className='flex items-center gap-4 w-full justify-center' onClick={() => setSelectedJob(topJobs[index])}>
+          <AdminCareerCard job={topJobs[index]} onPrev={prev} onNext={next} />
+        </div>
+      ) : (
+        <p className="text-center text-gray-500 mt-8">No jobs available.</p>
+      )}
+
+
 
       {/* Dots */}
       <div className='flex gap-1 mt-4'>
-        {jobPostings.map((_, i) => (
+        {topJobs.map((_, i) => (
           <span
             key={i}
             className={`w-2 h-2 rounded-full ${i === index ? 'bg-primary' : 'bg-gray-300'}`}
@@ -88,15 +127,15 @@ function AdminCareer() {
         <div className='w-full lg:w-auto min-w-xs ml-5'>
           {/* Open button */}
           <button className={`px-12 py-3 cursor-pointer border-b-3 w-1/2 lg:w-auto ${jobType === 'open' ? 'border-primary' : 'border-transparent'}`} onClick={() => setJobType('open')}>
-            <p className='text-black font-satoshi-medium text-md'> Open </p>
+            <p className='text-black font-satoshi-medium text-md'> Open ({openCount}) </p>
           </button>
-            {/* Closed button */}
+          {/* Closed button */}
           <button className={`px-12 py-3 cursor-pointer border-b-3 w-1/2 lg:w-auto ${jobType === 'closed' ? ' border-primary' : 'border-transparent'}`} onClick={() => setJobType('closed')}>
-            <p className='text-black font-satoshi-medium text-md'> Closed </p>
+            <p className='text-black font-satoshi-medium text-md'> Closed ({closedCount}) </p>
           </button>
             {/* Reported button */}
           <button className={`px-12 py-3 cursor-pointer border-b-3 w-1/2 lg:w-auto ${jobType === 'reported' ? ' border-primary' : 'border-transparent'}`} onClick={() => setJobType('reported')}>
-              <p className='text-black font-satoshi-medium text-md'> Reported </p>
+              <p className='text-black font-satoshi-medium text-md'> Reported ({reportedCount}) </p>
           </button>
         </div>
         {/* Sort by */}
@@ -139,35 +178,12 @@ function AdminCareer() {
       </div>
       {/* Table for desktop*/}
       <div className='border border-gray-400 rounded-xl p-6 flex-1 hidden lg:block overflow-auto w-full'>
-        <JobTable data={jobs}/>
-      </div>
-        <AdminModal isOpen={!!selectedJob} onClose={() => setSelectedJob(null)}>
-        {selectedJob && (
-          <>
-            <div className='h-1/3 w-full bg-primary font-satoshi-regular rounded-t-2xl'></div>
-            <div className='p-6'>
-                <h2 className="text-4xl font-satoshi-bold mb-2">{selectedJob.job_title}</h2>
-                <p className="text-2xl font-satoshi-regular">
-                {selectedJob.org || 'Institute of Computer Science, UPLB'}
-                </p>
-                <p className='font-satoshi-regular'>
-                    Posted by: <span className="text-primary">{selectedJob.creator}</span>
-                </p>
-                <p className='font-satoshi-regular'>
-                    Date: {selectedJob.date_posted}
-                </p>
-
-                <h3 className="font-satoshi-medium mb-1 text-lg">Details</h3>
-                <p className='font-satoshi-regular text-sm'>Details here</p>
-
-                <h3 className="font-satoshi-medium mb-1 text-lg">Description</h3>
-                <p className="font-satoshi-regular text-sm">
-                This is a placeholder description. Replace with actual data or enrich the job object!
-                </p>
-            </div>
-          </>
+        {loading ? (
+          <CircularLoading/>
+        ) : (
+          <JobTable data={jobs} jobType={jobType} />
         )}
-      </AdminModal>
+      </div>
     </div>
   );
 }
