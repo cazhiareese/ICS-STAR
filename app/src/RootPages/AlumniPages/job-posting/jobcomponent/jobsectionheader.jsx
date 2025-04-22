1a0faeada2307e920cb1d68c1cac1c9b8acb7161
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-22 20:06:39 +0800

    ui: center text on small devices

--- app/src/RootPages/AlumniPages/job-posting/jobcomponent/jobsectionheader.jsx
+++ app/src/RootPages/AlumniPages/job-posting/jobcomponent/jobsectionheader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function JobSectionHeader({ title }) {
   return (
-    <h2 className="text-[28px] sm:text-[40px] font-satoshi-bold sm:pl-8  text-gray-800 mb-4 text-left">
+    <h2 className="text-[28px] sm:text-[40px] font-satoshi-bold text-gray-800 mb-4 text-center sm:text-left sm:pl-13">
       {title}
     </h2>
   );
