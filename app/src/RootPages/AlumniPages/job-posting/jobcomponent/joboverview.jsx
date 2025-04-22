76d2b192ee03a19454f017df1cb204c532ad0b83
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-22 19:51:15 +0800

    feat: card component for job overview

--- app/src/RootPages/AlumniPages/job-posting/jobcomponent/joboverview.jsx
+++ app/src/RootPages/AlumniPages/job-posting/jobcomponent/joboverview.jsx
@@ -3,23 +3,23 @@ import { Heart, Calendar, User } from "lucide-react";
 
 function JobOverviewCard({ overview }) {
   return (
-    <div className="relative border border-gray-300 rounded-[20px] shadow-xl max-h-[178px]  py-6 px-8 w-full max-w-[1100px] mx-auto bg-white flex flex-row justify-between items-center mb-6">
+    <div className="relative border border-gray-300 rounded-[20px] shadow-xl max-h-[178px]  py-6 px-8 w-full max-w-[1100px] mx-auto  bg-white flex flex-row justify-between items-center mb-6">
       {/* Left Section: Role & Company */}
       <div className="flex flex-col">
-        <h2 className="text-[18px] text-primary font-satoshi-bold sm:text-[40px]">{overview.title}</h2>
-        <p className="font-satoshi-regular">{overview.company}</p>
-        <p className="font-satoshi-regular">{overview.poster_location || "—"}</p>
+        <h2 className="text-[18px] text-primary font-satoshi-black sm:text-[40px] sm:font-satoshi-bold">{overview.title}</h2>
+        <p className="text-[11px]c sm:text-[18px] font-satoshi-medium">{overview.company}</p>
+        <p className="text-[11px]c sm:text-[18px] font-satoshi-medium">{overview.poster_location || "—"}</p>
       </div>
 
       {/* Right Section: Stats */}
-      <div className="flex flex-col gap-2 text-sm text-gray-700 font-satoshi-regular">
+      <div className="flex flex-col gap-2 text-[11px] sm:text-[18px] text-black font-satoshi-medium">
         <div className="flex items-center gap-2">
           <Heart className="text-primary w-4 h-4" />
           <p>
-            <span className="text-primary font-satoshi-bold">
+            <span className="text-primary font-satoshi-black sm:font-satoshi-bold">
               {overview.total_interested} people
             </span>{" "}
-            are interested
+            are interested     
           </p>
         </div>
         <div className="flex items-center gap-2">
