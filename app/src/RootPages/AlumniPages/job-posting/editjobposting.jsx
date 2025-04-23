8255c4cb4150eb969f9ebfa766c87fd23e0d10f1
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-24 03:20:15 +0800

    feat: initialzied edit job posting page

---
+++ app/src/RootPages/AlumniPages/job-posting/editjobposting.jsx
@@ -0,0 +1,12 @@
+// EditJobPosting.jsx
+import React from "react";
+
+function EditJobPosting() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold">Edit Job Posting</h1>
+    </div>
+  );
+}
+
+export default EditJobPosting;
