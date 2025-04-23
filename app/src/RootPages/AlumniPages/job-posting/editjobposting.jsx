4c7292d05c872f053b65163c9ced2a84f896fa28
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-24 07:54:06 +0800

    feat: layout the input separation

--- app/src/RootPages/AlumniPages/job-posting/editjobposting.jsx
+++ app/src/RootPages/AlumniPages/job-posting/editjobposting.jsx
@@ -12,6 +12,33 @@ function EditJobPosting() {
       </div>
 
       <JobSectionHeader title="Edit Job Posting" />
+      <div className="max-w-[1100px] mx-auto bg-whitey rounded-[10px] shadow border border-disabled p-4">
+        <div className="flex flex-row gap-1 py-1">
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+        </div>
+        <div className="flex flex-row gap-1 py-1">
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+        </div>
+        <div className="flex flex-row gap-1 py-1">
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+          
+        </div>
+        <div className="flex flex-row gap-1 py-1">
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+          
+        </div>
+        <div className="flex flex-row gap-1 py-1">
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+          <div className="w-full border-neutral-c rounded-[11px] bg-white p-4 "></div>
+        </div>
+      </div>
+      <div className="text-right mt-2">
+        <button className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors">
+          Submit
+        </button>
+      </div>
       </div>
   );
 }
