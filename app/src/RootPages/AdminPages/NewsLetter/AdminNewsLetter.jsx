a11cdc4838abdd0e255562d777b993bcb52e968c
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-26 01:29:09 +0800

    chore: added dummy data for testing

--- app/src/RootPages/AdminPages/NewsLetter/AdminNewsLetter.jsx
+++ app/src/RootPages/AdminPages/NewsLetter/AdminNewsLetter.jsx
@@ -8,6 +8,24 @@ import { Navigate, useNavigate } from 'react-router-dom'
 
 
 function AdminNewsletter() {
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(48)
+  const [newsletter, setNewsletter] = useState([])
+
+  useEffect(() => {
+    const dummy = {
+      title: "Sample Title",
+      image: "https://via.placeholder.com/150",
+      date_posted: "2025-04-26",
+      context: "This is a sample context description for the dummy data."
+    };
+
+    // Duplicate the dummy data 5 times
+    const duplicatedData = Array(8).fill(dummy);
+    
+    setNewsletter(duplicatedData);
+  }, []);
+
   return (
     <div className='h-screen w-full p-6 flex flex-col'>
       {/* Events header and new event button */}
@@ -17,6 +35,23 @@ function AdminNewsletter() {
           <Plus/> New Newsletter
         </button>
       </div>
+      <div className='flex flex-col w-full lg:w-auto lg:flex-row items-center lg:justify-between lg:ml-5 gap-2 lg:gap-0'>
+
+        {/* Dito Search Bar implementation */}
+        <div className='w-full lg:w-auto  min-w-xs'></div>
+        <div className='items-center gap-2 text-md font-satoshi-regular hidden lg:flex'>
+          <MoveLeft className='cursor-pointer' onClick={() => {}}/>
+            <p> Page </p>
+            <input
+              type="text"
+              value={page}
+              onChange={() => {}}
+              className="w-9 text-center border border-disabled rounded-md outline-none text-primary font-satoshi-bold"
+            />
+            <p>of {totalPages}</p>
+            <MoveRight className='cursor-pointer' onClick={() => {}}/>
+          </div>
+        </div>
       </div>  
   )
 }
