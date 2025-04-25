a07aec994964beca230247a49f428833371149da
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-26 00:33:46 +0800

    chore: establish the button navigation and header

--- app/src/RootPages/AdminPages/NewsLetter/AdminNewsLetter.jsx
+++ app/src/RootPages/AdminPages/NewsLetter/AdminNewsLetter.jsx
@@ -9,9 +9,15 @@ import { Navigate, useNavigate } from 'react-router-dom'
 
 function AdminNewsletter() {
   return (
-    <h>
-      fsdfsdfsd
-    </h>
+    <div className='h-screen w-full p-6 flex flex-col'>
+      {/* Events header and new event button */}
+      <div className='flex flex-row justify-between mb-10'>
+        <h1 className='font-satoshi-bold text-5xl text-black'>Newsletter</h1>
+        <button className='flex flex-row items-center justify-center gap-2 font-satoshi-regular text-white bg-primary px-6 py-3 rounded-2xl hover:bg-hover cursor-pointer' onClick={() => {}}> 
+          <Plus/> New Newsletter
+        </button>
+      </div>
+      </div>  
   )
 }
 
