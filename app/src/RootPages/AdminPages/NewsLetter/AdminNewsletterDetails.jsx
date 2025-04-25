585a3846a130d4d5e6a33b23c41aa5b2124846a4
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-26 02:53:23 +0800

    chore: added dummy data for presentation

--- app/src/RootPages/AdminPages/NewsLetter/AdminNewsletterDetails.jsx
+++ app/src/RootPages/AdminPages/NewsLetter/AdminNewsletterDetails.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import { MoveLeft, Pencil, Trash2 } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
 function AdminNewsletterDetails() {
   const navigate = useNavigate();
+  const [newsletter, setNewsletter] = useState(null);
+
+  useEffect(() => {
+    const longContext = `Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+    
+    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+    
+    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibussem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+    
+    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.`;
+  
+    const dummy = {
+      title: 'Sample Title',
+      image: null,
+      date_posted: '2025-04-26',
+      context: longContext,
+      tags: ['Tag1', 'Tag2', 'Tag3', 'Tag4', 'Tag5'],
+      link: ['http://example.com', 'http://example2.com', 'http://example3.com']
+    };
+  
+    setNewsletter(dummy);
+  }, []);
+  
+  console.log("Newsletters:", newsletter); // Debugging line to check the newsletter object
 
   return (
     <div className="p-6 h-screen w-full flex flex-col">
