ffe7b9f0e047f9e9c10a20282ac4074c21b1956b
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-26 00:28:25 +0800

    chore: added temporary imports

--- app/src/RootPages/AdminPages/NewsLetter/AdminNewsLetter.jsx
+++ app/src/RootPages/AdminPages/NewsLetter/AdminNewsLetter.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Plus, MoveLeft, MoveRight, Dot } from 'lucide-react'
+import AdminEventCard from '../../../components/AdminComponents/AdminEventCard'
+import axios from 'axios'
+import EventsTable from '../../../components/AdminComponents/EventsTable'
+import CircularLoading from "../../../components/LoadingComponents/circularloading" 
+import { Navigate, useNavigate } from 'react-router-dom'
+
 
 function AdminNewsletter() {
   return (
