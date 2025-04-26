bd41df2e083132088bfd0627afa3d1c795af0a40
cazhiareese <clleva@up.edu.ph>
2025-04-27 05:12:40 +0800

    chore: integrated events search and sort

--- app/src/RootPages/AdminPages/Events/AdminEvents.jsx
+++ app/src/RootPages/AdminPages/Events/AdminEvents.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react'
-import { Plus, MoveLeft, MoveRight, Dot } from 'lucide-react'
+import { Plus, MoveLeft, MoveRight, Dot, Search } from 'lucide-react'
 import AdminEventCard from '../../../components/AdminComponents/AdminEventCard'
 import axios from 'axios'
 import EventsTable from '../../../components/AdminComponents/EventsTable'
 import CircularLoading from "../../../components/LoadingComponents/circularloading" 
 import { Navigate, useNavigate } from 'react-router-dom'
-
+import SortModal from '../../../components/AdminComponents/sortmodal'
+import OrderToggle from '../../../components/AdminComponents/ordertoggle'
 function AdminEvents() {
   const navigate = useNavigate()
   const [eventType, setEventType] = useState('active')
@@ -13,17 +14,53 @@ function AdminEvents() {
   const [totalPages, setTotalPages] = useState(48)
   const [loading, setLoading] = useState(false)
   const [events, setEvents] = useState([])
-
+   const [query, setQuery] = useState('')
+    const [focused, setFocused] = useState(false)
   const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
+
+
+  const sorters = [
+    {label: 'Date Concluded', value: 'concluded'},
+    {label: 'RSVP Count', value: 'count'}
+  ]
+
+  const [sortBy, setSortBy] = useState(sorters[0].value);
+  const [sortDirection, setSortDirection] = useState('desc');
+  const [orderBy, setOrderBy] = useState('concluded_desc');
+
+  const handleSortFieldChange = (field) => {
+    setSortBy(field);
+    const newParam = `${field}_${sortDirection}`;
+    setOrderBy(newParam);
+  };
+
+  const handleDirectionToggle = (newDirection) => {
+    setSortDirection(newDirection);
+    const newParam = `${sortBy}_${newDirection}`;
+    setOrderBy(newParam);
+  };
 
   async function fetchEvents(type, token) {
     setLoading(true)
     try {
+      console.log(query)
+      const params = new URLSearchParams();
+      if (query && query.length > 0) {
+        params.append('title', query);
+      }else{
+        params.delete('title');
+      }
+
+      if (orderBy && type==='finished') {
+        params.append('order_by', orderBy);
+      }
+
+      const queryString = params.toString();
       let endpoint = "";
       if (type === 'active') {
-        endpoint = "/api/admin/events/all-open-events";
+        endpoint = `/api/admin/events/all-open-events/?${queryString}`;
       } else if (type === 'finished') {
-        endpoint = "/api/admin/events/all-concluded-events";
+        endpoint = `/api/admin/events/all-concluded-events/?${queryString}`;
       }
   
       const response = await axios.get(`${API_BASE_URL}${endpoint}`,
@@ -41,7 +78,8 @@ function AdminEvents() {
   useEffect(() => {
     const token = localStorage.getItem("token")
     fetchEvents(eventType, token)
-  }, [eventType])
+  }, [eventType, query, sortBy, sortDirection])
+
 
   return (
     <div className='h-screen w-full p-6 flex flex-col bg-white'>
@@ -64,6 +102,28 @@ function AdminEvents() {
             <p className='text-black text-md'> Finished </p>
           </button>
         </div>
+
+        <div className='relative flex items-center justify-end flex-1'>
+            <input
+              type="text"
+              placeholder="Search"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              onFocus={() => setFocused(true)}
+              onBlur={() => setFocused(false)}
+              className={`w-full lg:w-xs px-4 py-2 border rounded-3xl focus:outline-none ${focused ? 'border-primary border-2': 'border-gray-400'}`}
+            />
+            <Search className={`absolute mr-2 ${focused ? 'text-primary' : 'text-gray-400'}`} size={20} />
+            
+          {eventType === 'finished'?  <>
+            <SortModal filters={sorters} selectedFilter={sortBy} onSelect={handleSortFieldChange}/>
+          
+            {/* Order by */}
+            <OrderToggle direction={sortDirection} onToggle={handleDirectionToggle}/>
+            </>: null }
+          </div>
+
+        
         {/* Page */}
         <div className='items-center gap-2 text-md font-satoshi-regular hidden lg:flex'>
           <MoveLeft className='cursor-pointer' onClick={() => {}}/>
