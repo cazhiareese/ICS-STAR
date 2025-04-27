54493bca944b778d5128e9373d3fb141581e420c
rfquejada <rfquejada@up.edu.ph>
2025-04-27 16:12:09 +0800

    refactor: added pagination to closed drives by amount asc

--- api/routers/admin_donations_route.py
+++ api/routers/admin_donations_route.py
@@ -225,16 +225,26 @@ def closed_drives_by_amount_raised(
         data=paginated_results
     )
 
-@router.get("/admin/donations/closed-drives-by-amount-raised-ascending", response_model=List[AdminClosedDonationDriveOut])
+@router.get("/admin/donations/closed-drives-by-amount-raised-ascending", response_model=PaginatedClosedDonationDrivesResponse)
 def closed_drives_by_amount_raised_ascending(
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    page: int = Query(1, ge=1, description="Page number")
 ):
     results = get_all_closed_drives_by_amount_raised_ascending(db)
 
+    page_size = 10
+
     if not results:
         raise HTTPException(status_code=404, detail="No closed drives found")
+    
+    total_pages, paginated_results = paginate_results(results, page, page_size)
 
-    return results
+    return PaginatedClosedDonationDrivesResponse(
+        message="success",
+        page=page,
+        total_pages=total_pages,
+        data=paginated_results
+    )
 
 @router.get("/admin/donations/closed-drives-by-date-closed-newest", response_model=List[AdminClosedDonationDriveOut])
 def closed_drives_by_date_closed_newest(
