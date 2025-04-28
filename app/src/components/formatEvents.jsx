f8fb5c756a91255a1056a1e58c89bd0823ea72fb
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-28 23:25:27 +0800

    chore: added dayjs and component for event format

---
+++ app/src/components/formatEvents.jsx
@@ -0,0 +1,31 @@
+import dayjs from 'dayjs'; // (make sure you have installed dayjs)
+
+const formatEvents = (eventList) => {
+  // group by days_left if multiple events
+  const grouped = {};
+
+  eventList.forEach(event => {
+    const dayLabel = event.days_left === 1
+      ? "Tomorrow"
+      : `In ${event.days_left} days`;
+
+    const timeFormatted = dayjs(event.date).format('h:mm A');
+
+    if (!grouped[dayLabel]) {
+      grouped[dayLabel] = [];
+    }
+
+    grouped[dayLabel].push({
+      title: event.title,
+      time: `${timeFormatted}, ${event.location}`
+    });
+  });
+
+  // convert grouped object into array
+  const events = Object.entries(grouped).map(([dateLabel, items]) => ({
+    dateLabel,
+    items
+  }));
+
+  return events;
+};
