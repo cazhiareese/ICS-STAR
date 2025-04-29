af9cfa089f221c150a9747f03e05b1846f3d4b5f
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-30 02:27:35 +0800

    ui: remove hovering

--- app/src/RootPages/Profile/About/skillsinterestsection.jsx
+++ app/src/RootPages/Profile/About/skillsinterestsection.jsx
@@ -41,7 +41,7 @@ const SkillsInterestsSection = ({
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 mt-4">
           {skills.map((skill, index) => (
             <div key={index} className="relative">
-              <span className="block px-4 py-2 border-2 border-primary text-primary rounded-full font-satoshi-bold text-[16px] hover:bg-hover hover:text-white transition text-center">
+              <span className="block px-4 py-2 border-2 border-primary text-primary rounded-full font-satoshi-bold text-[16px] text-center">
                 {skill}
               </span>
               {editMode && (
