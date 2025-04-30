ada76246dae3af94298fcfbe021092a3c4b7ad03
Cyrus Gello Par <cmpar@up.edu.ph>
2025-04-30 11:04:56 +0800

    chore: fixed the routing in mobile view

--- app/src/components/AlumniComponents/JobExpandedCard.jsx
+++ app/src/components/AlumniComponents/JobExpandedCard.jsx
@@ -265,9 +265,19 @@ import {
 
                             {/* Interested count TODO: Add onclick */}
                             <div className="flex items-center gap-1 pt-2 cursor-pointer">
-                                <span className="text-lg text-primary font-satoshi-bold underline hover:text-blue-700">
-                                    {job.interested_count} are interested
-                                </span>
+                            {job.user_id === currentUserID ? (
+  <button
+    onClick={() => navigate(`/alumni/jobPosting/interested/${jobId}`)}
+    className="text-lg text-primary font-satoshi-bold hover:hover cursor-pointer"
+  >
+    {job.interested_count} are interested
+  </button>
+) : (
+  <span className="text-lg text-primary font-satoshi-bold">
+    {job.interested_count} are interested
+  </span>
+)}
+
                             </div>
                         </div>
                         
