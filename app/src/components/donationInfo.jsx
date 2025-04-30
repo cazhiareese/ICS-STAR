36901fb438b5468c85d02fa797b25adfa0503585
Mar Paul Palacio <mrpalacio@up.edu.ph>
2025-04-30 11:35:16 +0800

    chore: fixed responsiveness of donation

--- app/src/components/donationInfo.jsx
+++ app/src/components/donationInfo.jsx
@@ -39,15 +39,18 @@ function DonationInfo({generalDrive, general }) {
       }
 
     return (
-        <div className="flex md:flex-row flex-col justify-center md:items-start items-center overflow-y-auto md:pt-20 pt-10 md:space-x-20">
-            <div className="flex flex-col lg:w-[45%] md:w-[40%] w-[95%] md:h-180 h-120 border border-gray-300 rounded-2xl items-center overflow-y-auto mb-5">
+        <div className="flex lg:flex-row flex-col justify-center lg:items-start items-center overflow-y-auto lg:pt-20 pt-10 lg:space-x-20 sm:mx-0 mx-5">
+            <div className="flex flex-col lg:w-[45%] md:w-[90%] w-[95%] md:h-180 sm:h-120 border border-gray-300 rounded-2xl overflow-y-auto mb-5">
                 {
                     generalDrive==null ? (<DonationMainView driveDetails={driveDetails} driveId = {driveid} landing={false}/>):(<DonationMainView driveDetails={generalDrive} driveId = {generalDrive.drive_id}/>)
                 }
             </div>
-                
+            <div className="lg:w-[30%]">
             {generalDrive==null ? (<DonationCard driveDetails={driveDetails} driveId = {driveid}/>):(<DonationCard driveDetails={generalDrive} driveId = {generalDrive.drive_id}/>)}
-        </div>
+       
+            </div>
+                
+             </div>
     );
 }
 
