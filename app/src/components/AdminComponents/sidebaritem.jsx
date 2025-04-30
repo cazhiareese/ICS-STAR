2257cf72e72fde032f28e0bdfe393cc542bd4959
kiefertayawa <kltayawa@up.edu.ph>
2025-05-01 01:10:58 +0800

    ui: adjust whole admin navbar

--- app/src/components/AdminComponents/sidebaritem.jsx
+++ app/src/components/AdminComponents/sidebaritem.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ChevronDown, ChevronUp } from "lucide-react";
+import { useLocation } from "react-router-dom";
 
 const SidebarItem = ({ title, icon, isSelected, childrenItems, path, onClick }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const location = useLocation();
 
   return (
     <div>
       {/* Main Row */}
       <div
-        className={`flex flex-row p-2 rounded-r-3xl items-center border-l-6 ${
+        className={`flex flex-row p-2 rounded-r-3xl items-center border-l-6 mr-3 ${
           isSelected ? "border-primary bg-blue-100 font-satoshi-bold" : "border-transparent hover:bg-gray-100 hover:text-hover"
         }`}
       >
@@ -40,17 +42,22 @@ const SidebarItem = ({ title, icon, isSelected, childrenItems, path, onClick }) 
 
       {/* Children Links */}
       {isExpanded && childrenItems && (
-        <div className="ml-8 mt-1 flex flex-col space-y-1">
-          {childrenItems.map((child, idx) => (
-            <Link
-              key={idx}
-              to={`/admin/${child.path}`}
-              className="text-black font-satoshi-light text-sm hover:text-primary hover:underline"
-              onClick={onClick}
-            >
-              {child.title}
-            </Link>
-          ))}
+        <div className="ml-15 mt-1 flex flex-col space-y-1">
+          {childrenItems.map((child, idx) => {
+            const isActive = location.pathname === `/admin/${child.path}`;
+            return (
+              <Link
+                key={idx}
+                to={`/admin/${child.path}`}
+                className={`pl-2 text-lg font-satoshi-regular hover:text-primary hover:underline ${
+                  isActive ? "text-primary font-satoshi-medium underline" : "text-black"
+                }`}
+                onClick={onClick}
+              >
+                {child.title}
+              </Link>
+            );
+          })}
         </div>
       )}
     </div>
