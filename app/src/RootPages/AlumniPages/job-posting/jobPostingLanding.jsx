1f033d873efe164196fa11a9bd4a502ebd037d3f
JanryDeoduco <janrydeoduco612@gmail.com>
2025-05-01 19:21:08 +0800

    feat: Added paginatiion for get all jobs

--- app/src/RootPages/AlumniPages/job-posting/jobPostingLanding.jsx
+++ app/src/RootPages/AlumniPages/job-posting/jobPostingLanding.jsx
@@ -31,6 +31,10 @@ function JobPostingLanding() {
 
     const [isError, setError] = useState(false);
     const [isAlumni, setIsAlumni] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [maxPage, setMaxPage] = useState(1);
     
 
     const toggleWorkType = (workType) => {
@@ -109,27 +113,26 @@ function JobPostingLanding() {
     useEffect(() => {
         const fetchJobs = async () => {
         setLoading(true);
-        try {
-            const response = await fetch(`${API_BASE_URL}/job-postings`);
-            if (!response.ok) {
-            throw new Error('Failed to fetch jobs');
-            }
-            const data = await response.json();
-            console.log(data)
-            setJobList(data);
-            setLoading(false);
-            setError(false);
-        } catch (err) {
-            console.log(err.message || 'Something went wrong');
-            setLoading(false);
-            setError(true);
-        } finally {
-            setLoading(false);
-        }
+            try {
+                const response = await fetch(`${API_BASE_URL}/job-postings/?page=${currentPage}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch jobs');
+                }
+                const data = await response.json();
+                setMaxPage(data.total_pages);
+                setJobList(data.result);
+            
+                setLoading(false);
+                setError(false);
+            } catch (err) {
+                console.log(err.message || 'Something went wrong');
+                setLoading(false);
+                setError(true);
+            } 
         };
 
         fetchJobs();
-    }, []);
+    }, [currentPage]);
 
     
 
@@ -245,6 +248,19 @@ function JobPostingLanding() {
             setLoading(false);
         } finally {
             setLoading(false);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < maxPage){
+            setCurrentPage((prevPage) => prevPage + 1);
+        }
+        
+    };
+
+    const handlePrevPage = () => {
+        if (currentPage > 1){
+            setCurrentPage((prevPage) => prevPage - 1);
         }
     };
     
@@ -442,44 +458,50 @@ function JobPostingLanding() {
             </div>
 
             <div className='flex flex-row mt-16 gap-2 justify-center'>
-                {/* Scrollable wrapper */}
-                <div className='h-[660px] overflow-y-scroll overflow-x-hidden pt-1 scrollbar-left w-xl outline-0'>
-                    {!loading ? (
-                        <div className='flex flex-col gap-5 items-center '>
-                            {/* Pagination */}
-                            <div className='flex flex-row gap-5 font-satoshi-medium text-lg'>
-                                <button className='cursor-pointer'><ArrowLeft/></button>
-                                <h1>Page</h1> 
-                                <div className='flex justify-center bg-blue-100 w-8 rounded-md'>
-                                    <h1 className='text-primary'>1</h1>
-                                </div> 
-                                <h1>of</h1> 
-                                <h1>80</h1> 
-                                <button className='cursor-pointer'><ArrowRight/></button>
+                <div className='flex flex-col'>
+                    <div className='flex flex-col items-center mb-16'>
+                        {/* Pagination */}
+                        <div className='flex flex-row gap-5 font-satoshi-medium text-lg'>
+                            <button onClick={handlePrevPage} className='cursor-pointer'><ArrowLeft/></button>
+                            <h1>Page</h1> 
+                            <div className='flex justify-center bg-blue-100 w-8 rounded-md'>
+                                <h1 className='text-primary'>{currentPage}</h1>
+                            </div> 
+                            <h1>of</h1> 
+                            <h1>{maxPage}</h1> 
+                            <button onClick={handleNextPage} className='cursor-pointer'><ArrowRight/></button>
+                        </div>
+                    </div>
+                    {/* Scrollable wrapper */}
+                    <div className='h-[660px] overflow-y-scroll overflow-x-hidden pt-1 scrollbar-left w-xl outline-0'>
+                        
+                        {!loading ? (
+                            <div className='flex flex-col gap-5 items-center '>
+                                
+                                {!isError && Array.isArray(jobList) && jobList.length > 0 ? (
+                                    jobList.map((job, index) => (
+                                        <JobCard
+                                        key={index} // Consider using job.id if available instead of index
+                                        job={job}
+                                        selectedJobId={selectedJobId}
+                                        setSelectedJobId={setSelectedJobId}
+                                        setMobileExpanded={setMobileExpanded}
+                                        
+                                        />
+                                    ))
+                                    ) : (
+                                    <p className="text-gray-500 text-center mt-4">
+                                        {isError ? 'No jobs found.' : 'No jobs available.'}
+                                    </p>
+                                )}
                             </div>
-                            {!isError && Array.isArray(jobList) && jobList.length > 0 ? (
-                                jobList.map((job, index) => (
-                                    <JobCard
-                                    key={index} // Consider using job.id if available instead of index
-                                    job={job}
-                                    selectedJobId={selectedJobId}
-                                    setSelectedJobId={setSelectedJobId}
-                                    setMobileExpanded={setMobileExpanded}
-                                    
-                                    />
-                                ))
-                                ) : (
-                                <p className="text-gray-500 text-center mt-4">
-                                    {isError ? 'No jobs found.' : 'No jobs available.'}
-                                </p>
-                            )}
-                        </div>
-                    ) : (
-                        <div className='flex flex-row justify-center h-full gap-5'>
-                            <h1 className='text-xl font-satoshi-bold text-gray-400'> Loading Jobs</h1>
-                            <CircularLoading />
-                        </div>
-                    )}
+                        ) : (
+                            <div className='flex flex-row justify-center h-full gap-5'>
+                                <h1 className='text-xl font-satoshi-bold text-gray-400'> Loading Jobs</h1>
+                                <CircularLoading />
+                            </div>
+                        )}
+                    </div>
                 </div>
 
 
