888e4f8f9c552bdccb4c9ae0ac1782143e010d14
Markus Recaplaza <markusrecaplaza@Markuss-MacBook-Air.local>
2025-05-02 00:19:51 +0800

    fixed add links in create newsletter

--- app/src/components/AdminComponents/YearPickerModal.jsx
+++ app/src/components/AdminComponents/YearPickerModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import DatePicker from "react-multi-date-picker";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import colors from "react-multi-date-picker/plugins/colors"
 import { X } from "lucide-react";
 import "./YearPickerCss.css";
 
@@ -34,7 +33,7 @@ const DatePickerModal = ({ setIsDatePickerModalOpen, setDateList }) => {
         </div>
 
         {/* Content: Date Picker */}
-        <div className="flex flex-col items-center w-full mb-6">
+        <div className="flex flex-col items-center mb-6">
           {/* DatePicker Component */}
           <DatePicker
             multiple
@@ -45,7 +44,15 @@ const DatePickerModal = ({ setIsDatePickerModalOpen, setDateList }) => {
             sort
             inputClass="border border-gray-300 rounded-xl px-3 h-10 py-2 md:w-4xs w-full text-left focus:border-primary focus:outline-none focus:ring-0 pl-10 pr-10"
             calendarClassName="p-4 w-sm"
-            colors={[colors("#FFFFFF")]}
+            style={{
+              width: "100%",
+              boxSizing: "border-box",
+              height: "35px"
+            }}
+            containerStyle={{
+              width: "100%"
+            }}
+            calendarPosition="bottom-center"
             renderButton={(direction, handleClick) => (
               <button onClick={handleClick}>
                 {direction === "right" ? (
