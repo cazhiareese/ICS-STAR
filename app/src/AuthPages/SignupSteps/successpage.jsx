63f7de3df8cca15d9618b19aebcd9f3d9977c3ba
kiefertayawa <kltayawa@up.edu.ph>
2025-05-04 12:09:18 +0800

    ui: adjusted login error modal and success page button

--- app/src/AuthPages/SignupSteps/successpage.jsx
+++ app/src/AuthPages/SignupSteps/successpage.jsx
@@ -107,12 +107,12 @@
                         
                 </div>
 
-                <div class=" flex flex-col items-end lg:w-150 md:w-100 w-70 mt-6 mb-57">
+                <div class=" flex flex-col items-end lg:w-150 md:w-100 w-70 mt-6 mb-57 cursor-pointer">
                 {loading ? (
                     <Loading /> // Show loading component while registration is in progress
                 ) : (
                     <button
-                        className="bg-primary text-white py-3 rounded-3xl text-lg w-40 font-bold hover:bg-blue-700 transition mt-6 mb-50"
+                        className="bg-primary text-white py-3 rounded-3xl text-lg w-40 font-bold hover:bg-blue-700 transition mt-6 mb-50 cursor-pointer"
                         onClick = {()=>(handleNextPage())}
                     >
                         Continue
--- app/src/AuthPages/modaltemplate.jsx
+++ app/src/AuthPages/modaltemplate.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ModalTemplate = ({ onClose, onContinue, choiceclose, choicecontinue, header, information }) => {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 bg-opacity-10 z-50">
-            <div className="bg-white rounded-lg shadow-lg p-6 w-96">
+            <div className="bg-white rounded-3xl shadow-lg p-6 w-96">
                 <h2 className="text-xl font-bold mb-4 text-center">{header}</h2>
                 <p className="text-gray-600 text-center mb-6">
                     {information}
@@ -12,7 +12,7 @@ const ModalTemplate = ({ onClose, onContinue, choiceclose, choicecontinue, heade
                 <div className="flex justify-center space-x-10">
                     {choiceclose &&
                         <button
-                        className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300"
+                        className="px-4 py-2 bg-gray-200 text-gray-700 rounded-2xl hover:bg-gray-300 cursor-pointer font-regular"
                         onClick={onClose}
                     >
                         {choiceclose}
@@ -20,7 +20,7 @@ const ModalTemplate = ({ onClose, onContinue, choiceclose, choicecontinue, heade
                     }
                     {choicecontinue &&
                         <button
-                            className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+                            className="px-4 py-2 bg-green-500 text-white rounded-2xl hover:bg-green-600 font-regular"
                             onClick={onContinue}
                         >
                             {choicecontinue}
