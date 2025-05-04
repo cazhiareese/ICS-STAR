d2c0d88e52b036edaa1247cbf873f2d22e705219
Cyrus Gello Par <cmpar@up.edu.ph>
2025-05-04 17:20:59 +0800

    chore: adjusted the tabs at small devices

--- app/src/RootPages/Profile/components/userprofiletabs.jsx
+++ app/src/RootPages/Profile/components/userprofiletabs.jsx
@@ -5,7 +5,7 @@ const tabs = ["About", "Work", "Job Posted", "Donation History"];
 function UserProfileTabs({ userDetails, editMode, activeTab, setActiveTab }) {
   return (
     <div
-  className={`w-full max-w-[1100px] border border-disabled rounded-[10px] ${userDetails.is_verified ? 'bg-whitey' : 'bg-white'} p-2 flex flex-wrap justify-center sm:justify-start gap-7 sm:gap-7 mt-4 px-6`}
+  className={`w-full max-w-[1100px] border border-disabled rounded-[10px] ${userDetails.is_verified ? 'bg-whitey' : 'bg-white'} p-2 flex flex-wrap justify-center sm:justify-start gap-4  sm:gap-7 mt-4 px-6`}
 >
 {tabs.map((tab) => (
   <span
@@ -15,7 +15,7 @@ function UserProfileTabs({ userDetails, editMode, activeTab, setActiveTab }) {
         setActiveTab(tab);
       }
     }}
-    className={`relative font-satoshi-medium text-[18px] sm:text-[20px] leading-[30px] tracking-[-0.02em] transition cursor-pointer
+    className={`relative font-satoshi-medium text-[17px] sm:text-[20px] leading-[30px] tracking-[-0.02em] transition cursor-pointer
       ${
         editMode || !userDetails.is_verified
           ? "pointer-events-none text-gray-400 opacity-50"
